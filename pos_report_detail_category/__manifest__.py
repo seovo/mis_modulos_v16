1d5b3434accd7df6be5de9ce34740c95f3515ed7
JESUS <you@example.com>
2023-09-17 18:44:26 -0500

    oka

---
+++ pos_report_detail_category/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Report Detail Category",
+    'summary': """Report Detail Category """,
+
+    'description': """Report Detail Category""",
+    'author': "JESUS",
+    'website': "",
+
+    'category': 'Uncategorized',
+    'version': '0.1',
+    # any module necessary for this one to work correctly
+    'depends': ['point_of_sale'],
+
+    # always loaded
+    'data': [
+        'report_saledetails.xml'
+    ],
+
+    # 'images': ['static/description/odoo-woo.gif'],
+}
