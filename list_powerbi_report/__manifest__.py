0fb846f607588fcc5c6efff75f649338f94a6a44
JESUS <you@example.com>
2024-03-24 14:59:06 -0500

    powerbi

--- list_powerbi_report/__manifest__.py
+++ list_powerbi_report/__manifest__.py
@@ -16,7 +16,7 @@
         "views/template.xml",
     ],
     # 'uninstall_hook': 'uninstall_hook',
-    "external_dependencies": {"python": ["msal"]},
+    "external_dependencies": {"python": ["msal","pymssql","pandas"]},
     "application": False,
     "installable": True,
     "auto_install": False,
--- list_powerbi_report/models/list_powerbi.py
+++ list_powerbi_report/models/list_powerbi.py
@@ -25,6 +25,34 @@ class ListPowerBi(models.Model):
     report_id = fields.Char()
 
     def powerbi_ui(self):
+        import pandas as pd
+        import pymssql
+        #import psycopg2
+        #import psycopg2.extras
+
+        # Function to fetch data from SQL Server data table
+        def fetch_data_from_sql_server(server, database, username, password, table_name):
+            connection = pymssql.connect(server=server, database=database, user=username, password=password)
+
+            query = f'SELECT * FROM {table_name};'
+            data = pd.read_sql(query, connection)
+            connection.close()
+
+            return data
+
+            # Configuration for SQL Server
+        sql_server_config = {
+                "server": "dbserverdatawave.database.windows.net",
+                "database": "Datawave",
+                "username": "datawaveuser",
+                "password": "JT5j]6u2?XZzdw4fS#CK[pWBH!QxsD$t",
+                "table_name": "res_partner",
+        }
+
+        data = fetch_data_from_sql_server(**sql_server_config)
+
+        raise ValueError(data)
+        return
         self.ensure_one()
 
         # self.rename_sequence_shf()
