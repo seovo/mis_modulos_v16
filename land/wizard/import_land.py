24a22d5e76766da734db2d2d59f912d80e645cb0
JESUS <you@example.com>
2024-04-09 17:19:51 -0500

    land

--- land/wizard/import_land.py
+++ land/wizard/import_land.py
@@ -51,7 +51,7 @@ class ImportCiHrAttendance(models.TransientModel):
 
         for index, row in ventas.iterrows():
 
-            if c > 200:
+            if c > 300:
                 break
             c += 1
 
