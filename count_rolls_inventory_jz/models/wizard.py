edbc088c18680c937406b5b99569540caf9726ea
JESUS <you@example.com>
2024-07-25 23:27:35 -0500

    ok

--- count_rolls_inventory_jz/models/wizard.py
+++ count_rolls_inventory_jz/models/wizard.py
@@ -8,6 +8,13 @@ class ProductWizardVariant(models.TransientModel):
     line_ids      = fields.One2many('product.wizard.variant.line','parent_id')
     sale_id       = fields.Many2one('sale.order')
 
+    @api.onchange('sale_id')
+    def change_sale(self):
+        pt = self.env['product.template'].search([('attribute_line_ids','!=',False)],limit=1)
+        if not pt:
+            self.product = pt.id
+
+
     @api.onchange('product')
     def change_product(self):
         self.line_ids = False
