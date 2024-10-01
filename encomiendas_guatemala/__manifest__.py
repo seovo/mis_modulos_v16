1f474926faf020f86c0ec8184ef3f40820c5dbf7
JESUS <you@example.com>
2024-10-01 01:00:12 -0500

    update

---
+++ encomiendas_guatemala/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Encomiendas Guatemala",
+    "summary": "Encomiendas Guatemala",
+    "description": """Encomiendas Guatemala""",
+    "author": "Jzolutions",
+    "category": "Uncategorized",
+    "version": "17.0",
+    "depends": ["website_sale"],
+    "data": [],
+    # 'uninstall_hook': 'uninstall_hook',
+    #"external_dependencies": {"python": ["msal","pymssql","pandas"]},
+    "application": False,
+    "installable": True,
+    "auto_install": False,
+
+}
---
+++ encomiendas_guatemala/controllers/main.py
@@ -0,0 +1,57 @@
+from odoo.addons.payment.controllers import portal as payment_portal
+from odoo import fields, http, SUPERUSER_ID, tools, _
+from odoo.http import request, route
+from werkzeug.exceptions import Forbidden, NotFound
+
+class WebsiteSale(payment_portal.PaymentPortal):
+    @http.route(['/shop/cart'], type='http', auth="public", website=True, sitemap=False)
+    def cart(self, access_token=None, revive='', **post):
+        """
+        Main cart management + abandoned cart revival
+        access_token: Abandoned cart SO access token
+        revive: Revival method when abandoned cart. Can be 'merge' or 'squash'
+        """
+        order = request.website.sale_get_order()
+        raise ValueError(order)
+        if order and order.carrier_id:
+            # Express checkout is based on the amout of the sale order. If there is already a
+            # delivery line, Express Checkout form will display and compute the price of the
+            # delivery two times (One already computed in the total amount of the SO and one added
+            # in the form while selecting the delivery carrier)
+            order._remove_delivery_line()
+        if order and order.state != 'draft':
+            request.session['sale_order_id'] = None
+            order = request.website.sale_get_order()
+
+        request.session['website_sale_cart_quantity'] = order.cart_quantity
+
+        values = {}
+        if access_token:
+            abandoned_order = request.env['sale.order'].sudo().search([('access_token', '=', access_token)], limit=1)
+            if not abandoned_order:  # wrong token (or SO has been deleted)
+                raise NotFound()
+            if abandoned_order.state != 'draft':  # abandoned cart already finished
+                values.update({'abandoned_proceed': True})
+            elif revive == 'squash' or (revive == 'merge' and not request.session.get(
+                    'sale_order_id')):  # restore old cart or merge with unexistant
+                request.session['sale_order_id'] = abandoned_order.id
+                return request.redirect('/shop/cart')
+            elif revive == 'merge':
+                abandoned_order.order_line.write({'order_id': request.session['sale_order_id']})
+                abandoned_order.action_cancel()
+            elif abandoned_order.id != request.session.get(
+                    'sale_order_id'):  # abandoned cart found, user have to choose what to do
+                values.update({'access_token': abandoned_order.access_token})
+
+        values.update({
+            'website_sale_order': order,
+            'date': fields.Date.today(),
+            'suggested_products': [],
+        })
+        if order:
+            order.order_line.filtered(lambda l: not l.product_id.active).unlink()
+            values['suggested_products'] = order._cart_accessories()
+            values.update(self._get_express_shop_payment_values(order))
+
+        values.update(self._cart_values(**post))
+        return request.render("website_sale.cart", values)
