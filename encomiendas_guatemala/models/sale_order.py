d67e3c971a63cfee1f1fe894820b5df9b6d26cdf
JESUS <you@example.com>
2024-10-03 16:05:49 -0500

    upt

--- encomiendas_guatemala/models/sale_order.py
+++ encomiendas_guatemala/models/sale_order.py
@@ -4,8 +4,8 @@ from odoo.exceptions import UserError
 class SaleOrder(models.Model):
     _inherit       = 'sale.order'
     state_encomienda = fields.Selection([
-        ('1','=','Transito Caja Rural al Cenam'),
-        ('2','=','Despacho a USA'),
-        ('3', '=', 'Salio a Aduana'),
-        ('4', '=', 'Entregado'),
+        ('1','Transito Caja Rural al Cenam'),
+        ('2','Despacho a USA'),
+        ('3', 'Salio a Aduana'),
+        ('4', 'Entregado'),
     ])
