9340fdac8450d0c6617a85988dd48b4f197b0f83
JESUS <you@example.com>
2024-10-19 15:05:18 -0500

    update

--- encomiendas_guatemala/controllers/main.py
+++ encomiendas_guatemala/controllers/main.py
@@ -16,7 +16,10 @@ class WebsiteSale(payment_portal.PaymentPortal):
         authorized_fields = request.env['ir.model']._get('res.partner')._get_form_writable_fields()
         #raise ValueError(authorized_fields)
         for k, v in values.items():
-            raise ValueError([k,authorized_fields])
+            if k in ['use_whatsapp']:
+                new_values[k] = v
+
+            #raise ValueError([k,authorized_fields])
             # don't drop empty value, it could be a field to reset
             if k in authorized_fields and v is not None:
                 new_values[k] = v
