7164dc37007b2b0a0796bbf56a89527a0900c09e
JESUS <you@example.com>
2024-10-19 12:07:14 -0500

    update

--- encomiendas_guatemala/models/res_partner.py
+++ encomiendas_guatemala/models/res_partner.py
@@ -4,3 +4,7 @@ from odoo.exceptions import UserError
 class ResPartner(models.Model):
     _inherit       = 'res.partner'
     use_whatsapp = fields.Boolean(string='Tiene Wasap')
+
+    def write(self,values):
+        res = super().write(values)
+        return res
