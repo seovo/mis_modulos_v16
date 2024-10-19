ac737625b26413e5b75f194313f127c0fda330ec
JESUS <you@example.com>
2024-10-19 15:00:03 -0500

    update

--- encomiendas_guatemala/controllers/main.py
+++ encomiendas_guatemala/controllers/main.py
@@ -14,6 +14,7 @@ class WebsiteSale(payment_portal.PaymentPortal):
     def values_postprocess(self, order, mode, values, errors, error_msg):
         new_values = {}
         authorized_fields = request.env['ir.model']._get('res.partner')._get_form_writable_fields()
+        raise ValueError(authorized_fields)
         for k, v in values.items():
             # don't drop empty value, it could be a field to reset
             if k in authorized_fields and v is not None:
