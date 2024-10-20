9c9bf31feb8e451f7146e04e6b3f96915b4e186a
JESUS <you@example.com>
2024-10-20 14:19:25 -0500

    update

--- encomiendas_guatemala/models/sale_order.py
+++ encomiendas_guatemala/models/sale_order.py
@@ -71,6 +71,7 @@ class SaleOrderLine(models.Model):
     encomienda_list = fields.One2many('sale.order.line.encomienda.list','sale_order_line_id')
     is_encomienda   = fields.Boolean(related='product_id.is_encomienda')
     price_fixed     = fields.Float(compute='change_items_encomienda',string='Precio Fijo')
+    price_total_encomienda =  fields.Float(compute='change_items_encomienda',string='Precio Total Encomienda')
 
     @api.onchange('encomienda_list','encomienda_list.amount_total')
     def change_items_encomienda(self):
@@ -88,7 +89,7 @@ class SaleOrderLine(models.Model):
                     total_peso_cobro = total_peso_cobro - 1
                     total_peso_cobro = total_peso_cobro * record.product_id.price_extra_libre
 
-
+                record.price_total_encomienda = total_price
                 record.price_fixed = record.product_id.list_price
                 record.price_unit = record.price_fixed + total_peso_cobro + total_price
 
