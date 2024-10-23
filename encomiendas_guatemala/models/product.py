f42821a498159dd5bad06e6f219af262c1a521cb
JESUS <you@example.com>
2024-10-23 16:27:34 -0500

    update

--- encomiendas_guatemala/models/product.py
+++ encomiendas_guatemala/models/product.py
@@ -5,3 +5,6 @@ class ProductTemplate(models.Model):
     is_encomienda = fields.Boolean(string="Es un Producto Encomienda")
     is_tarif_encomienda = fields.Boolean(string="Es Tarifa Encomienda")
     price_extra_libre = fields.Float(string="Precio Extra Libra")
+    largo_enc = fields.Float(string="Largo")
+    alto_enc = fields.Float(string="Alto")
+    ancho_enc = fields.Float(string="Largo")
