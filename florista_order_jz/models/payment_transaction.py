724108e37764bbea2e30c431c752956f66881aef
JESUS <you@example.com>
2024-11-07 02:11:42 -0500

    update

--- florista_order_jz/models/payment_transaction.py
+++ florista_order_jz/models/payment_transaction.py
@@ -29,11 +29,13 @@ class PaymentTransaction(models.Model):
 
 
         order = self.sale_order_ids
-        if not order.customer_order_delivery_date:
-            raise ValueError('Se requiere fecha de envio')
+
 
         """ Function to fetch the values of the payment gateway"""
         res = super()._get_specific_rendering_values(processing_values)
+
+        if not order.customer_order_delivery_date:
+            raise ValueError('Se requiere fecha de envio')
 
         return res
 
