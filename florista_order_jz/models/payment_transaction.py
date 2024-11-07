00550e58447f5a8a5ad28b2f895b517d2afcc1aa
JESUS <you@example.com>
2024-11-07 02:33:46 -0500

    update

--- florista_order_jz/models/payment_transaction.py
+++ florista_order_jz/models/payment_transaction.py
@@ -44,6 +44,8 @@ class PaymentTransaction(models.Model):
         if order.customer_order_delivery_date <= date_order:
             raise ValueError('La fecha de envio debe ser mayor a la fecha actual')
 
+        raise ValueError([order.customer_order_delivery_date,date_order])
+
         return res
 
         if self.provider_code != 'mercadopagolink':
