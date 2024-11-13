213675edf095ee94955c77e420664a3cdbaf1900
JESUS <you@example.com>
2024-11-12 20:12:25 -0500

    update

--- report_tiempos_sale_line_jz/models/sale_order.py
+++ report_tiempos_sale_line_jz/models/sale_order.py
@@ -16,16 +16,14 @@ class SaleOrderLine(models.Model):
     def get_data_report_time(self):
         for record in self:
 
-            record.move_reference = record.move_ids>[0].reference if record.move_ids else None
-            record.purchase_id = record.purchase_line_ids[0].order_id.id if record.purchase_line_ids else None
-            record.purchase_create_date = record.purchase_line_ids[0].order_id.create_date if record.purchase_line_ids else None
-            record.purchase_date_order = record.purchase_line_ids[
-                0].order_id.date_order if record.purchase_line_ids else None
-            record.purchase_create_uid = record.purchase_line_ids[
-                0].order_id.create_uid.id if record.purchase_line_ids else None
-            record.purchase_product_qty = record.purchase_line_ids[
-                0].product_qty if record.purchase_line_ids else None
-            record.purchase_qty_received = record.purchase_line_ids[
-                0].qty_received if record.purchase_line_ids else None
-            record.purchase_product_uom_qty = record.purchase_line_ids[
-                0].product_uom_qty if record.purchase_line_ids else None
+            record.move_reference = record.move_ids[0].reference if record.move_ids else None
+
+            purchase_line = record.move_ids[0].created_purchase_line_id if record.move_ids else None
+
+            record.purchase_id = purchase_line.order_id.id if purchase_line else None
+            record.purchase_create_date = purchase_line.order_id.create_date if purchase_line else None
+            record.purchase_date_order = purchase_line.order_id.date_order if purchase_line else None
+            record.purchase_create_uid = purchase_line.order_id.create_uid.id if purchase_line else None
+            record.purchase_product_qty = purchase_line.product_qty if purchase_line else None
+            record.purchase_qty_received = purchase_line.qty_received if purchase_line else None
+            record.purchase_product_uom_qty = purchase_line.product_uom_qty if purchase_line else None
