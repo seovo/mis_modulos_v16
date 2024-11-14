8d4e6bfe1d762081b03dbefcb312f6a976ff73ee
JESUS <you@example.com>
2024-11-14 11:19:22 -0500

    update

--- florista_order_jz/models/payment_transaction.py
+++ florista_order_jz/models/payment_transaction.py
@@ -25,47 +25,4 @@ class PaymentTransaction(models.Model):
     #)
 
 
-    def _get_specific_rendering_values(self, processing_values):
-
-
-
-
-
-        """ Function to fetch the values of the payment gateway"""
-        res = super()._get_specific_rendering_values(processing_values)
-
-        order = self.sale_order_ids
-
-        if not order.customer_order_delivery_date:
-            raise ValueError('Se requiere fecha de envio')
-
-        date_order = order.date_order.date()
-
-        if order.customer_order_delivery_date <= date_order:
-            raise ValueError('La fecha de envio debe ser mayor a la fecha actual')
-
-        return res
-
-        if self.provider_code != 'mercadopagolink':
-            return res
-
-
-        link = None
-
-        for line in self.sale_order_ids.order_line:
-            if line.product_id.sale_ok and line.product_id.link_mercado_pago:
-                link = line.product_id.link_mercado_pago
-
-        if not  link:
-            raise ValueError('No se Encontro Link de Pago')
-
-
-
-
-        return {
-            'api_url': link,
-            #'headers': headers
-            #'data': data,
-        }
-
 
