0fdf65467a303e3fbf48a8b10dcee07bfc90bfbd
JESUS <you@example.com>
2024-11-18 01:36:59 -0500

    update

--- florista_order_jz/models/product_attribute.py
+++ florista_order_jz/models/product_attribute.py
@@ -1,5 +1,18 @@
 from odoo import _, api, fields, models
 
+class ProductProduct(models.Model):
+    _inherit = 'product.product'
+    product_florista_ids = fields.One2many('product.product.florista','product_id')
+
+
+class ProductProductFlorista(models.Model):
+    _inherit = 'product.product.florista'
+    sequence = fields.Integer()
+    product_id = fields.Many2one('product.product')
+    product_terminado_id = fields.Many2one('product.product',string="Producto Terminado")
+
+
+
 class ProductAttribute(models.Model):
     _inherit = 'product.attribute'
     is_period_florista = fields.Boolean(string='Es Periodo Flores')
