ba7e90b5ae1c989824389a30444d55b27e96dd37
JESUS <you@example.com>
2024-11-19 22:48:59 -0500

    update

--- florista_order_jz/models/sale_order.py
+++ florista_order_jz/models/sale_order.py
@@ -26,7 +26,7 @@ class SaleOrder(models.Model):
 
 
     def add_envio_florista(self,c=0):
-        if self.date_shipment_florista:
+        if self.product_terminado_florista:
             suscripcion = None
             for line in self.order_line:
                 if line.price_unit != 0:
