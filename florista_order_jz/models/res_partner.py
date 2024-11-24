c7876adba343327bf2e24775d14485e64be857d6
JESUS <you@example.com>
2024-11-23 22:56:11 -0500

    update

--- florista_order_jz/models/res_partner.py
+++ florista_order_jz/models/res_partner.py
@@ -4,5 +4,6 @@ class ResPartner(models.Model):
     _inherit = 'res.partner'
     date_shipment_florista = fields.Date(string='Fecha Envio Flores')
     until_hour_florista = fields.Many2one('until.hour.florista', string="Entregar Hasta")
+    email_mercadopago = fields.Char(string="Email Mercado Pago")
 
 
