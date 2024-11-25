fa27aca91a1c9a563ae3e213429b16e3ea2a1934
JESUS <you@example.com>
2024-11-25 18:00:54 -0500

    update

--- florista_order_jz/models/sale_order.py
+++ florista_order_jz/models/sale_order.py
@@ -58,6 +58,11 @@ class SaleOrder(models.Model):
 
                         florista_ids = value.product_attribute_value_id.product_florista_ids
 
+                        array_florista= []
+
+                        for fl in florista_ids:
+                            array_florista.append(fl.id)
+
 
                         for product_f in  florista_ids + florista_ids + florista_ids:
 
@@ -73,7 +78,7 @@ class SaleOrder(models.Model):
                                     'price_unit': 0,
                                     'tax_id': None
                                 })
-                                self.next_order_number_list = florista_ids.index(product_f.product_terminado_id) + 1
+                                self.next_order_number_list = florista_ids.index(product_f.product_terminado_id.id) + 1
                                 ctt += 1
 
                             ct += 1
