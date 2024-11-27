a037800c8caed24b53e02642f99477fb908ffad4
JESUS <you@example.com>
2024-11-27 00:08:12 -0500

    update

--- florista_order_jz/models/sale_order.py
+++ florista_order_jz/models/sale_order.py
@@ -37,7 +37,7 @@ class SaleOrder(models.Model):
     def add_envio_florista(self,c=0):
 
         #raise ValueError(self.product_terminado_florista)
-        if self.product_terminado_florista:
+        if self.product_terminado_florista and self.id_vex:
             suscripcion = None
             for line in self.order_line:
                 if line.price_unit != 0:
