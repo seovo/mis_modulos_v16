52c07ec0cf6457f3cd194669bfc58efc7188412b
JESUS <you@example.com>
2024-12-12 11:27:45 -0500

    update

--- florista_order_jz/models/sale_order.py
+++ florista_order_jz/models/sale_order.py
@@ -12,12 +12,12 @@ class UntilHourFlorista(models.Model):
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
-    date_shipment_florista = fields.Date(string='Fecha Envio Flores')
+    date_shipment_florista = fields.Date(string='Primera fecha de entrega')
     until_hour_florista = fields.Many2one('until.hour.florista',string="Entregar Hasta")
 
-    number_period_florista = fields.Integer(string="Nro Periodo", compute="get_data_florista",store=True)
+    number_period_florista = fields.Integer(string="Número de cobro", compute="get_data_florista",store=True)
     interval_period_florista = fields.Integer(string="Dias Intervalo", compute="get_data_florista",store=True)
-    product_terminado_florista = fields.Many2one('product.product', compute="get_data_florista")
+    product_terminado_florista = fields.Many2one('product.product', compute="get_data_florista",string='Producto Suscripción')
     next_order_number_list = fields.Integer(string="Siguiente # de Producto Terminado")
     product_attribute_value_florista_id = fields.Many2one('product.attribute.value', compute="get_data_florista")
 
