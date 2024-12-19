81464e643c27c43670b0e65789bfa9bf0989b5b0
JESUS <you@example.com>
2024-12-19 12:12:35 -0500

    update

---
+++ migrate_version_odoo_jz/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Migrador de Versiones de Odoo",
+    "summary": "Migrador de Versiones de Odoo",
+    "description": """Migrador de Versiones de Odoo""",
+    "author": "Jzolutions",
+    "category": "Uncategorized",
+    "version": "17.0",
+    "depends": ["sale_management","l10n_pe_edi_odoofact"],
+    "data": [
+    ],
+    # 'uninstall_hook': 'uninstall_hook',
+    #"external_dependencies": {"python": ["msal"]},
+    "application": True,
+    "installable": True,
+    "auto_install": False,
+
+}
