097d0040853d7d0bfbd3407f93a6165ecd62ecd0
JESUS <you@example.com>
2024-12-20 12:47:19 -0500

    update

--- migrate_version_odoo_jz/models/sale.py
+++ migrate_version_odoo_jz/models/sale.py
@@ -16,7 +16,7 @@ class SaleOrder(models.Model):
 
         # Configuración de conexión
         host = 'localhost'  # Cambia esto por la dirección de tu servidor
-        port = 432  # Puerto
+        port = 5432  # Puerto
         dbname = 'villasur'  # Nombre de la base de datos
         user = 'odoo'  # Tu usuario
         password = 'RVFERo%gE65ZJcpf4Xz%'  # Tu contraseña
