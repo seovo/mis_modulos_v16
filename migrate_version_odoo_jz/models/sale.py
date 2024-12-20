7d14d73133ee33bc0208e9bb9654de2c59bdb78f
JESUS <you@example.com>
2024-12-20 13:41:11 -0500

    update

--- migrate_version_odoo_jz/models/sale.py
+++ migrate_version_odoo_jz/models/sale.py
@@ -39,12 +39,33 @@ class SaleOrder(models.Model):
 
 
 
-        cursor.execute("SELECT * FROM res_partner;")
 
+
+
+        select_columns = [
+            'id',
+            'company_id',
+            'create_date',
+            'name',
+            'title',
+            'parent_id',
+            'user_id',
+            'state_id',
+            'country_id',
+            'industry_id',
+            'color',
+            'commercial_partner_id',
+            'create_uid',
+            'write_uid',
+            #'display_name',
+            'ref', 'lang', 'tz', 'vat', 'company_registry', 'website', 'function', 'type', 'street', 'street2', 'zip', 'city', 'email', 'phone', 'mobile', 'commercial_company_name', 'company_name', 'date', 'comment', 'partner_latitude', 'partner_longitude', 'active', 'employee', 'is_company', 'partner_share', 'write_date', 'message_main_attachment_id', 'message_bounce', 'email_normalized', 'contact_address_complete', 'signup_type', 'signup_expiration', 'signup_token', 'team_id', 'ocn_token', 'partner_gid', 'additional_info', 'phone_sanitized', 'supplier_rank', 'customer_rank', 'invoice_warn', 'invoice_warn_msg', 'debit_limit', 'last_time_entries_checked', 'sale_warn', 'sale_warn_msg', 'city_id', 'street_name', 'street_number', 'street_number2', 'l10n_latam_identification_type_id', 'l10n_pe_district', 'online_partner_information', 'followup_reminder_type', 'purchase_warn', 'purchase_warn_msg', 'picking_warn', 'picking_warn_msg']
+
+        string_columns = ",".join(select_columns)
+        cursor.execute(f"SELECT {select_columns} FROM res_partner;")
         resultados = cursor.fetchall()  # Obtener todos los resultados
         column_names = [desc[0] for desc in cursor.description]
 
-        raise ValueError(column_names)
+        #raise ValueError(column_names)
 
 
 
