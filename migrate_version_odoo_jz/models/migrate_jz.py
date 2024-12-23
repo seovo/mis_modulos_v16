04afd96ad1cca9ac6e1c7258515a8b296465bd44
JESUS <you@example.com>
2024-12-22 23:05:31 -0500

    update

--- migrate_version_odoo_jz/models/migrate_jz.py
+++ migrate_version_odoo_jz/models/migrate_jz.py
@@ -165,6 +165,7 @@ class MigrateModelJz(models.Model):
             try:
                 self.env.cr.execute(SQL_INSERT, fila)
             except:
+                raise ValueError(fila)
                 sql_strr = "SELECT * FROM product_template  "
                 self.env.cr.execute(sql_strr)
                 result = self.env.cr.fetchall()
