bde1d810eedcb10664dd538ef731ad9ed3336d21
JESUS <you@example.com>
2024-12-22 23:43:15 -0500

    update

--- migrate_version_odoo_jz/models/migrate_jz.py
+++ migrate_version_odoo_jz/models/migrate_jz.py
@@ -168,12 +168,12 @@ class MigrateModelJz(models.Model):
             SQL_INSERT = f"INSERT INTO {table} ({val1}) VALUES ({val2}) ON CONFLICT (id) DO UPDATE SET {val3}"
 
             # raise ValueError([len(fila),])
-            self.env.cr.execute(SQL_INSERT, (f'''"{fila[0]}"'''))
+            #self.env.cr.execute(SQL_INSERT, (f'''"{fila[0]}"'''))
             '''
             self.env.cr.execute(SQL_INSERT, fila)
             '''
             try:
-                self.env.cr.execute(SQL_INSERT, (f'''"{fila[0]}"'''))
+                self.env.cr.execute(SQL_INSERT, [f'''"{fila[1]}"'''])
             except:
                 raise ValueError([SQL_INSERT, fila])
                 #raise ValueError([fila,SQL_INSERT])
