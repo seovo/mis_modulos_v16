42b1c5d38d17fac24b302c38954e699ff2139a91
JESUS <you@example.com>
2024-12-22 22:12:01 -0500

    update

--- migrate_version_odoo_jz/models/migrate_jz.py
+++ migrate_version_odoo_jz/models/migrate_jz.py
@@ -238,7 +238,7 @@ class MigrateModelJz(models.Model):
         resultados = cursor.fetchall()  # Obtener todos los resultados
         #raise ValueError(resultados)
 
-        # raise ValueError(column_names)
+        raise ValueError(column_names)
 
         n = len(column_names)  # Cambia este valor a la cantidad de {} que deseas
         corchetes_n = ','.join('%s' for _ in range(n))
