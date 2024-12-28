c139d370234f241707d61a98b4f8f4a65e12c656
JESUS <you@example.com>
2024-12-28 01:42:37 -0500

    update

--- compare_variant_jz/models/product.py
+++ compare_variant_jz/models/product.py
@@ -14,3 +14,18 @@ class ProductProduct(models.Model):
     new_price_comparation = fields.Float(
         'Precio Comparación', store=True,
         digits='Product Price')
+
+
+class ProductTemplate(models.Model):
+    _inherit = "product.template"
+
+    set_attribute_price = fields.Boolean(string='Set Attribute Price', default=False)
+
+
+
+    def get_combination_info(self, combination=False, product_id=False, add_qty=1, pricelist=False,
+                             parent_combination=False, only_template=False):
+        res = super().get_combination_info(combination, product_id, add_qty, pricelist,
+                             parent_combination, only_template)
+        raise ValueError(res)
+        return res
