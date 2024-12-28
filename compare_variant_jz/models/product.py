df20f9134fd2d81930973f180756c19cad0b386f
JESUS <you@example.com>
2024-12-28 02:01:13 -0500

    update

--- compare_variant_jz/models/product.py
+++ compare_variant_jz/models/product.py
@@ -21,11 +21,9 @@ class ProductTemplate(models.Model):
 
 
 
-
-
-    def get_combination_info(self, combination=False, product_id=False, add_qty=1, pricelist=False,
+    def _get_combination_info(self, combination=False, product_id=False, add_qty=1, pricelist=False,
                              parent_combination=False, only_template=False):
-        res = super().get_combination_info(combination, product_id, add_qty, pricelist,
+        res = super()._get_combination_info(combination, product_id, add_qty, pricelist,
                              parent_combination, only_template)
         raise ValueError(res)
         return res
