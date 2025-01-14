13e24fb8f6d9866794a66268e20ac09aa43bb36a
JESUS <you@example.com>
2025-01-14 10:15:21 -0500

    update

--- land/models/account_journal.py
+++ land/models/account_journal.py
@@ -4,3 +4,6 @@ class AccountJournal(models.Model):
     _inherit = 'account.journal'
     code_l10n_latam_document_type_id = fields.Char(related='l10n_latam_document_type_id.code')
     journal_reverse_jz = fields.Many2one('account.journal', string='Diario Reversa x Defecto')
+
+    def action_import_transaction_villasur(self):
+        return
