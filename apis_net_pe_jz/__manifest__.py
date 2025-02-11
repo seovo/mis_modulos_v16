60556fac1a6eb5a0ab53d4722a695e99bbe90131
JESUS <you@example.com>
2025-02-11 15:48:25 -0500

    up

---
+++ apis_net_pe_jz/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Api Rest de consultas RUC, DNI y Tipo de cambio ",
+    "summary": "Api Rest de consultas RUC, DNI y Tipo de cambio ",
+    "description": """Api Rest de consultas RUC, DNI y Tipo de cambio """,
+    "author": "Jzolutions",
+    "category": "Uncategorized",
+    "version": "1.1",
+    "depends": ["contacts","l10n_pe"],
+    "data": [
+        "views/view_company_form.xml",
+    ],
+    # 'uninstall_hook': 'uninstall_hook',
+    #"external_dependencies": {"python": ["msal"]},
+    "application": False,
+    "installable": True,
+    "auto_install": False,
+
+}
