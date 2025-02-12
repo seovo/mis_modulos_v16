3e2b441b0f290c00a677dea8c24d3e368cb7bf5d
JESUS <you@example.com>
2025-02-12 01:56:58 -0500

    up

--- api_mercadoclinicosas_jz/controller/main.py
+++ api_mercadoclinicosas_jz/controller/main.py
@@ -10,11 +10,38 @@ from odoo.tools import lazy, str2bool
 from odoo.addons.payment.controllers.post_processing import PaymentPostProcessing
 
 _logger = logging.getLogger(__name__)
-
+from odoo.exceptions import AccessError
+import odoo
 
 class ApiClinicos(http.Controller):
 
+    @http.route(['/apiclinicos/validate_login'], type='json', auth="public", methods=['POST'], website=True, csrf=False)
+    def index_validate_login(self, **post):
+        redirect = None
+        values = {}
 
+        return  request.session.authenticate(request.db, request.params['login'], request.params['password'])
+
+        try:
+            uid = request.session.authenticate(request.db, request.params['login'], request.params['password'])
+            values.update({
+                'login_success': True
+            })
+            #request.params['login_success'] = True
+            #return request.redirect(self._login_redirect(uid, redirect=redirect))
+        except odoo.exceptions.AccessDenied as e:
+            if e.args == odoo.exceptions.AccessDenied().args:
+                values.update({
+                    'error': "Incorrecto usuario/contraseña"
+                })
+                #values['error'] = _("Wrong login/password")
+            else:
+                values.update({
+                    'error': e.args[0]
+                })
+                #values['error'] = e.args[0]
+
+        return values
 
     @http.route('/apiclinicos/websites', type="json", auth='public',
                 website=True, methods=['POST', 'GET'], csrf=False, save_session=False)
