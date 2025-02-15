b6cccbd649e196c2966c4509c7cac23a2c62cd87
JESUS <you@example.com>
2025-02-14 23:42:42 -0500

    up

---
+++ report_project_clinicosas_jz/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Report Proyecto Clinica Cosas",
+    "summary": "Report Proyecto Clinica Cosas",
+    "description": """Report Proyecto Clinica Cosas""",
+    "author": "Jzolutions",
+    "category": "Uncategorized",
+    "version": "17.0",
+    "depends": ["project"],
+    "data": [
+        "views/report.xml",
+    ],
+    # 'uninstall_hook': 'uninstall_hook',
+    #"external_dependencies": {"python": ["msal"]},
+    "application": True,
+    "installable": True,
+    "auto_install": False,
+
+}
