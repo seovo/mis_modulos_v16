1b5a5a0b3038a7f9fef04ad9b86d836dddb0ad6b
JESUS <you@example.com>
2025-02-16 18:32:47 -0500

    up

--- migrate_version_odoo_jz/models/migrate_jz.py
+++ migrate_version_odoo_jz/models/migrate_jz.py
@@ -185,7 +185,7 @@ class MigrateModelJz(models.Model):
 
     def insert_record_migrate(self,cursor,table,column_names):
         resultados = cursor.fetchall()  # Obtener todos los resultados
-        raise ValueError(resultados)
+        #raise ValueError(resultados)
 
         #raise ValueError(column_names)
 
