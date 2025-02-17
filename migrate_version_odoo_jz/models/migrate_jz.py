75e7349990917af4e0dc9258af74e6d53db2e3c2
JESUS <you@example.com>
2025-02-16 21:21:58 -0500

    up

--- migrate_version_odoo_jz/models/migrate_jz.py
+++ migrate_version_odoo_jz/models/migrate_jz.py
@@ -211,7 +211,7 @@ class MigrateModelJz(models.Model):
             # AND  product_tmpl_id  = {product.product_tmpl_id.id}
 
             self.env.cr.execute(sql)
-            data =  self.env.cr.fetchall()
+            data =  self._cr.fetchone()
             raise ValueError(data)
 
 
