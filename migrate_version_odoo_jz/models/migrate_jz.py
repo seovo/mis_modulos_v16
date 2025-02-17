de1975a52b4630cc284e22d139fac9fb6ccdbce2
JESUS <you@example.com>
2025-02-16 19:49:18 -0500

    up

--- migrate_version_odoo_jz/models/migrate_jz.py
+++ migrate_version_odoo_jz/models/migrate_jz.py
@@ -222,7 +222,7 @@ class MigrateModelJz(models.Model):
             #self.env.cr.execute(SQL_INSERT, [fila[0],f'''"{fila[1]}"'''])
 
 
-            raise ValueError([SQL_INSERT,fila])
+            #raise ValueError([SQL_INSERT,fila])
 
 
             self.env.cr.execute(SQL_INSERT, fila)
