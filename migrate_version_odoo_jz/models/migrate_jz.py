25c020d84f437861c9dbdf8b00f23a111ba6d0d9
JESUS <you@example.com>
2025-02-16 22:26:29 -0500

    up

--- migrate_version_odoo_jz/models/migrate_jz.py
+++ migrate_version_odoo_jz/models/migrate_jz.py
@@ -221,14 +221,14 @@ class MigrateModelJz(models.Model):
                        INSERT INTO product_variant_combination(product_product_id,product_template_attribute_value_id)
                        VALUES ({fila[0]},{data[0][0]}); 
                 '''
-                try:
-                    self.env.cr.execute(SQL_INSERT)
-                except:
-                    pass
+                #try:
+                #    self.env.cr.execute(SQL_INSERT)
+                #except:
+                #    pass
 
                 SQL_INSERT = f'''
                     INSERT INTO product_attribute_value_product_template_attribute_line_rel(product_attribute_value_id,product_template_attribute_line_id)
-                    VALUES ({data[0][0]},{data[0][1]}); 
+                    VALUES ({fila[1]},{data[0][1]}); 
                 '''
 
                 try:
