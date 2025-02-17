13786c36eeb5486c987220c85f0e5783e17bd19d
JESUS <you@example.com>
2025-02-16 19:12:01 -0500

    up

--- migrate_version_odoo_jz/models/migrate_jz.py
+++ migrate_version_odoo_jz/models/migrate_jz.py
@@ -218,7 +218,7 @@ class MigrateModelJz(models.Model):
                     SQL_INSERT = f"INSERT INTO {table} ({val1}) VALUES ({val2}) ON CONFLICT (id) DO NOTHING"
 
 
-            #raise ValueError([len(fila),fila])
+            raise ValueError([len(fila),fila])
             #self.env.cr.execute(SQL_INSERT, [fila[0],f'''"{fila[1]}"'''])
 
 
