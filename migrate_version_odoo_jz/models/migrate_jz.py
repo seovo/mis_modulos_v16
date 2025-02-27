a90cb57d9bc4aff7546304bd0ad65fc8b7e9dfd1
JESUS <you@example.com>
2025-02-27 09:02:16 -0500

    up

--- migrate_version_odoo_jz/models/migrate_jz.py
+++ migrate_version_odoo_jz/models/migrate_jz.py
@@ -27,7 +27,7 @@ class MigrateJz(models.Model):
     def update_images(self):
         import requests
         import base64
-        products = self.env['product.product'].search([('image_1920','=',False)],limit=10)
+        products = self.env['product.product'].search([('image_1920','=',False)],limit=30)
 
         for product in products:
             image_url = f"http://34.176.22.205:8069/web/image?model=product.product&id={product.id}&field=image"
