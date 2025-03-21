7e4a7c92997b07a08a812da234e8ae455718826e
JESUS <you@example.com>
2025-03-21 16:56:16 -0500

    up

--- whatsapp_api_evolution_jz/models/mail_compose_message.py
+++ whatsapp_api_evolution_jz/models/mail_compose_message.py
@@ -3,6 +3,8 @@ import requests
 from odoo.exceptions import ValidationError
 from datetime import datetime, timedelta
 import requests
+import ast
+import json
 
 class MailComposeMessage(models.TransientModel):
     _inherit = "mail.compose.message"
@@ -11,27 +13,58 @@ class MailComposeMessage(models.TransientModel):
 
     def action_send_mail(self):
 
-        raise ValueError(self.res_ids)
+        #raise ValueError(self.res_ids)
 
-        object = self.env[self.model].search([('id','in',0)])
+        array = ast.literal_eval(self.res_ids)
+
+        objects = self.env[self.model].search([('id','in',array)])
+
+        responses = []
+
+        for object in objects:
+            if self.is_whatsapp_evolution_api:
+                token = '2Qrlw2jjp30P7CGFlcSo1FkJ5SX27X'
+
+                phone = object.phone or object.mobile
+
+                if not phone:
+                    raise ValidationError('No se indico Telefono')
+
+                phone = phone.replace('+','')
+                phone = phone.replace(' ','')
+
+                raise ValueError(phone)
+
+                if len(phone) != 11 :
+                    raise ValidationError(f'No tiene los digitos suficientes {phone}')
+
+
+
+
+                headers = {
+                    "Accept": "application/json",
+                    "Content-Type": "application/json",
+                    "Authorization": f"Bearer {token}",
+                }
+                url = 'https://xalachi.qr.xalachi.com/api/message/send-text'
+                data = {
+                    # "number": "123456789",
+                    "number": "50664307914",
+                    "message": "Plain text message"
+                }
+                res = requests.post(url, json=data, headers=headers)
+
+                response = res.json()
+
+                json_response = json.dumps(response, indent=4)
+
+                responses.append(str(json_response))
+
+        if responses:
+            self.body = str(responses)
 
         res = super().action_send_mail()
 
-        if self.is_whatsapp_evolution_api:
-            token = '2Qrlw2jjp30P7CGFlcSo1FkJ5SX27X'
-            headers = {
-                "Accept": "application/json",
-                "Content-Type": "application/json" ,
-                "Authorization": f"Bearer {token}",
-            }
-            url = 'https://xalachi.qr.xalachi.com/api/message/send-text'
-            data = {
-                #"number": "123456789",
-                "number": "50664307914",
-                "message": "Plain text message"
-            }
-            res = requests.post(url, json=data, headers=headers)
 
-            raise ValueError(res.json())
 
         return res
