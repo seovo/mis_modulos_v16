746d49ab0b04b613c756fd11c21b835bc1943852
JESUS <you@example.com>
2025-04-07 13:44:23 -0500

    up

--- encomiendas_guatemala/models/res_partner.py
+++ encomiendas_guatemala/models/res_partner.py
@@ -7,6 +7,10 @@ class ResPartner(models.Model):
     file_vat     = fields.Binary(string="Adjunto Documento")
     name_file_vat     = fields.Char()
     dpi = fields.Char()
+    name_invoice = fields.Char(string="Nombre Factura")
+    adress_invoice = fields.Char(string="Dirección Factura")
+    date_born = fields.Date(string="Fecha Nacimiento")
+    is_destinatario = fields.Boolean()
 
     @api.model
     def default_get(self, default_fields):
