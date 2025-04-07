57b226c9525fb088018280ce52db318f8e9b2ab8
JESUS <you@example.com>
2025-04-07 03:01:07 -0500

    up

--- encomiendas_guatemala/models/res_partner.py
+++ encomiendas_guatemala/models/res_partner.py
@@ -7,6 +7,19 @@ class ResPartner(models.Model):
     file_vat     = fields.Binary(string="Adjunto Documento")
     name_file_vat     = fields.Char()
 
+    @api.model
+    def default_get(self, default_fields):
+        """Add the company of the parent as default if we are creating a child partner.
+        Also take the parent lang by default if any, otherwise, fallback to default DB lang."""
+        values = super().default_get(default_fields)
+
+
+
+        # protection for `default_type` values leaking from menu action context (e.g. for crm's email)
+        values['is_company'] = False
+
+        return values
+
     def write(self,values):
         #raise ValueError(values)
         res = super().write(values)
