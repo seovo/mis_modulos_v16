0dd45c011aef2706f9dd4874094ef2a32e89d7ee
JESUS <you@example.com>
2025-05-03 17:35:46 -0500

    up

--- land/models/account_move.py
+++ land/models/account_move.py
@@ -1,5 +1,24 @@
 from odoo import api, fields, models , _
 from odoo.tools import float_is_zero, format_amount, format_date, html_keep_url, is_html_empty
+
+
+from odoo.tools import (
+    date_utils,
+    email_re,
+    email_split,
+    float_compare,
+    float_is_zero,
+    float_repr,
+    format_amount,
+    format_date,
+    formatLang,
+    frozendict,
+    get_lang,
+    groupby,
+    index_exists,
+    is_html_empty,
+)
+
 CURRENCY = {
     "PEN": 1,
     "USD": 2,
@@ -429,6 +448,55 @@ class AccountMove(models.Model):
             })
         return values
 
+
+    #override
+    @api.depends('move_type', 'line_ids.amount_residual')
+    def _compute_payments_widget_reconciled_info(self):
+        for move in self:
+            payments_widget_vals = {'title': _('Less Payment'), 'outstanding': False, 'content': []}
+
+            if move.state == 'posted' and move.is_invoice(include_receipts=True):
+                reconciled_vals = []
+                reconciled_partials = move.sudo()._get_all_reconciled_invoice_partials()
+                for reconciled_partial in reconciled_partials:
+                    counterpart_line = reconciled_partial['aml']
+                    if counterpart_line.move_id.ref:
+                        #reconciliation_ref = '%s (%s)' % (counterpart_line.move_id.name, counterpart_line.move_id.ref)
+                        reconciliation_ref = counterpart_line.move_id.ref
+                    else:
+                        reconciliation_ref = counterpart_line.move_id.name
+                    if counterpart_line.amount_currency and counterpart_line.currency_id != counterpart_line.company_id.currency_id:
+                        foreign_currency = counterpart_line.currency_id
+                    else:
+                        foreign_currency = False
+
+                    reconciled_vals.append({
+                        'name': counterpart_line.name,
+                        'journal_name': counterpart_line.journal_id.name,
+                        'company_name': counterpart_line.journal_id.company_id.name if counterpart_line.journal_id.company_id != move.company_id else None,
+                        'amount': reconciled_partial['amount'],
+                        'currency_id': move.company_id.currency_id.id if reconciled_partial['is_exchange'] else
+                        reconciled_partial['currency'].id,
+                        'date': counterpart_line.date,
+                        'partial_id': reconciled_partial['partial_id'],
+                        'account_payment_id': counterpart_line.payment_id.id,
+                        'payment_method_name': counterpart_line.payment_id.payment_method_line_id.name,
+                        'move_id': counterpart_line.move_id.id,
+                        'ref': reconciliation_ref,
+                        # these are necessary for the views to change depending on the values
+                        'is_exchange': reconciled_partial['is_exchange'],
+                        'amount_company_currency': formatLang(self.env, abs(counterpart_line.balance),
+                                                              currency_obj=counterpart_line.company_id.currency_id),
+                        'amount_foreign_currency': foreign_currency and formatLang(self.env,
+                                                                                   abs(counterpart_line.amount_currency),
+                                                                                   currency_obj=foreign_currency)
+                    })
+                payments_widget_vals['content'] = reconciled_vals
+
+            if payments_widget_vals['content']:
+                move.invoice_payments_widget = payments_widget_vals
+            else:
+                move.invoice_payments_widget = False
 
 class BankOrigin(models.Model):
     _name = 'bank.origin'
