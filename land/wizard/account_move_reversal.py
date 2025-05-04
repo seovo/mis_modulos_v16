475fe964f3ded56a243281bc9e24b41b2ca8416c
JESUS <you@example.com>
2025-05-04 02:37:35 -0500

    up

--- land/wizard/account_move_reversal.py
+++ land/wizard/account_move_reversal.py
@@ -3,10 +3,13 @@ from odoo import api, fields, models , _
 class AccountMoveReversal(models.TransientModel):
     _inherit = 'account.move.reversal'
 
-    @api.depends('move_ids')
-    def _compute_from_moves(self):
-        res = super()._compute_from_moves()
+
+    def _prepare_default_reversal(self, move):
+        """ Set the default document type and number in the new revsersal move taking into account the ones selected in
+        the wizard """
+        res = super()._prepare_default_reversal(move)
         res.update({
             'l10n_pe_edi_cancel_reason':  self.reason
         })
         return res
+
