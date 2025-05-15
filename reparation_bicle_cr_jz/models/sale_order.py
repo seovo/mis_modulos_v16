473a26c514de4d74d1e06071085a8827e0ece604
JESUS <you@example.com>
2025-05-15 00:38:31 -0500

    reportes ple

--- reparation_bicle_cr_jz/models/sale_order.py
+++ reparation_bicle_cr_jz/models/sale_order.py
@@ -15,20 +15,20 @@ class SaleOrder(models.Model):
     def action_quotation_send(self):
         """ Opens a wizard to compose an email, with relevant mail template loaded by default """
         self.ensure_one()
-        self.order_line._validate_analytic_distribution()
+        #self.order_line._validate_analytic_distribution()
         lang = self.env.context.get('lang')
-        mail_template = self._find_mail_template()
-        if mail_template and mail_template.lang:
-            lang = mail_template._render_lang(self.ids)[self.id]
+        #mail_template = self._find_mail_template()
+        #if mail_template and mail_template.lang:
+        #    lang = mail_template._render_lang(self.ids)[self.id]
         ctx = {
             'default_model': 'sale.order',
             'default_res_ids': self.ids,
-            'default_template_id': mail_template.id if mail_template else None,
+            #'default_template_id': mail_template.id if mail_template else None,
             'default_composition_mode': 'comment',
-            'mark_so_as_sent': True,
-            'default_email_layout_xmlid': 'mail.mail_notification_layout_with_responsible_signature',
-            'proforma': self.env.context.get('proforma', False),
-            'force_email': True,
+            #'mark_so_as_sent': True,
+            #"'default_email_layout_xmlid': 'mail.mail_notification_layout_with_responsible_signature',
+            #'proforma': self.env.context.get('proforma', False),
+            #'force_email': True,
             'model_description': self.with_context(lang=lang).type_name,
             'default_is_whatsapp_evolution_api': True
 
