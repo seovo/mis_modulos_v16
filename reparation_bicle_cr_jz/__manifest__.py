29ec35dfeab18d08d4506af60d1db86af7e7b51c
JESUS <you@example.com>
2025-05-18 01:24:28 -0500

    UP

--- reparation_bicle_cr_jz/__manifest__.py
+++ reparation_bicle_cr_jz/__manifest__.py
@@ -11,7 +11,7 @@
         "security/ir.model.access.csv",
         'views/sale_order.xml',
         'views/view_company_form.xml',
-        'views/menu.xml',
+        #'views/menu.xml',
         #'views/report.xml',
 
     ],
