f900c5514e787661fa01c37ed4507adfc4fc8193
JESUS <you@example.com>
2025-05-18 01:23:13 -0500

    UP

--- reparation_bicle_cr_jz/__manifest__.py
+++ reparation_bicle_cr_jz/__manifest__.py
@@ -11,8 +11,8 @@
         "security/ir.model.access.csv",
         'views/sale_order.xml',
         'views/view_company_form.xml',
-        #'views/menu.xml',
-        'views/report.xml',
+        'views/menu.xml',
+        #'views/report.xml',
 
     ],
     # 'uninstall_hook': 'uninstall_hook',
