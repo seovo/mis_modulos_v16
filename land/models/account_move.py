b4ba633a1c59f2ca9c8adc80468f984d05a47c1e
JESUS <you@example.com>
2025-05-21 17:30:48 -0500

    up

--- land/models/account_move.py
+++ land/models/account_move.py
@@ -124,6 +124,11 @@ class AccountMove(models.Model):
 
         '''
 
+
+    def update_all_moves(self):
+        moves = self.env['account.move'].search([])
+        moves.get_proveedores_land()
+
     @api.depends('invoice_line_ids','invoice_line_ids.sale_line_ids')
     def get_proveedores_land(self):
         for record in self:
