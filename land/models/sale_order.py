d926fc7c0dc6a45fd2dbab6a01d3c7e94ebdf4be
JESUS <you@example.com>
2025-05-30 14:13:21 -0500

    up

--- land/models/sale_order.py
+++ land/models/sale_order.py
@@ -474,7 +474,7 @@ class SaleOrder(models.Model):
     @api.onchange('order_line','mz_lot','sector','order_line.price_unit',
                   'order_line.product_uom_qty','note','report_lot_land_line_id','report_lot_land_line_id.zona',
                   'report_lot_land_line_id.manzana','report_lot_land_line_id.name','report_lot_land_line_id.area',
-                  'report_lot_land_line_id.ettapa','report_lot_land_line_id.price_m2',
+                  'report_lot_land_line_id.ettapa','report_lot_land_line_id.price_m2'
                   )
     def get_info_land(self):
         for record in self:
