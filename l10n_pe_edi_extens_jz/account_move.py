b36a3dfeffc580f460720ca07b12e749ea232f9c
JESUS <you@example.com>
2025-06-02 21:41:51 -0500

    up

--- l10n_pe_edi_extens_jz/account_move.py
+++ l10n_pe_edi_extens_jz/account_move.py
@@ -2,11 +2,36 @@ from odoo import _, api, fields, models
 
 class AccountMove(models.Model):
     _inherit = 'account.move'
+
+
+    def action_post(self):
+        if len(self) == 1:
+            if self.l10n_pe_edi_operation_type == '1001'  :
+                note = self.narration
+                self.narration = None
+
+        res = super().action_post()
+
+        if len(self) == 1:
+            if self.l10n_pe_edi_operation_type == '1001'  :
+                self.narration = note
+
+
+        return  res
+
+
     def button_process_edi_web_services(self):
-        note = self.note
-        self.note = None
+        if len(self) == 1:
+            if self.l10n_pe_edi_operation_type == '1001'  :
+                note = self.narration
+                self.narration = None
+
         res = super().button_process_edi_web_services()
-        self.note = note
+
+
+        if len(self) == 1:
+            if self.l10n_pe_edi_operation_type == '1001'  :
+                self.narration = note
 
 
         return  res
