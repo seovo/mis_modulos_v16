42225a99e415a4cc0fde78e1d4c0e9e63ffc6b55
JESUS <you@example.com>
2025-06-02 22:09:28 -0500

    up

--- l10n_pe_edi_extens_jz/account_move.py
+++ l10n_pe_edi_extens_jz/account_move.py
@@ -17,16 +17,15 @@ class AccountMove(models.Model):
 
     def button_process_edi_web_services(self):
         if len(self) == 1:
-            if self.l10n_pe_edi_operation_type == '1001'  :
-                note = self.narration
-                self.narration = None
+            note = self.narration
+            self.narration = None
+
 
         res = super().button_process_edi_web_services()
 
+        if self.l10n_pe_edi_operation_type == '1001':
+            self.narration = note
 
-        if len(self) == 1:
-            if self.l10n_pe_edi_operation_type == '1001'  :
-                self.narration = note
 
 
         return  res
