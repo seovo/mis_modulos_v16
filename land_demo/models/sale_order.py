ebf6f0617c39a7dc1e88eff1b0880980ccb69a81
JESUS <you@example.com>
2025-06-24 19:41:00 -0500

    UP~

--- land_demo/models/sale_order.py
+++ land_demo/models/sale_order.py
@@ -467,8 +467,8 @@ class SaleOrder(models.Model):
                 if line.product_id.payment_land_dues:
 
                     for line_inv in line.invoice_lines:
-                        if line_inv.move_id.l10n_pe_edi_reversal_type_id:
-                            continue
+                        #if line_inv.move_id.l10n_pe_edi_reversal_type_id:
+                        #    continue
 
                         if line_inv.move_id.payment_state ==  'reversed':
                             continue
