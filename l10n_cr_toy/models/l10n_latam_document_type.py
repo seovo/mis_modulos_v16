35dae7d40e87bca88a3dd74b83b935967b51fc4f
JESUS <you@example.com>
2025-08-06 18:57:21 -0500

    oka

--- l10n_cr_toy/models/l10n_latam_document_type.py
+++ l10n_cr_toy/models/l10n_latam_document_type.py
@@ -3,5 +3,9 @@ from odoo import api, fields, models
 
 class ResPartner(models.Model):
     _inherit = 'l10n_latam.identification.type'
+    code_sirett = fields.Integer()
 
+
+class L10n_latamDocumentType(models.Model):
+    _inherit = 'l10n_latam.document.type'
     code_sirett = fields.Integer()
