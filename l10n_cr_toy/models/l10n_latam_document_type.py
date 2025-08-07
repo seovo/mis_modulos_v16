ef90325b6dc54fc6f1ffb50079f75b848cb59a55
JESUS <you@example.com>
2025-08-06 19:47:47 -0500

    up

--- l10n_cr_toy/models/l10n_latam_document_type.py
+++ l10n_cr_toy/models/l10n_latam_document_type.py
@@ -6,6 +6,6 @@ class ResPartner(models.Model):
     code_sirett = fields.Integer()
 
 
-class L10n_latamDocumentType(models.Model):
-    _inherit = 'l10n_latam.document.type'
-    code_sirett = fields.Integer()
+#class L10n_latamDocumentType(models.Model):
+#    _inherit = 'l10n_latam.document.type'
+#    code_sirett = fields.Integer()
