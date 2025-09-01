8435ccd4a7199f74a1aa2feb380d000b8da4939f
JESUS <you@example.com>
2025-08-31 21:09:39 -0500

    UP

---
+++ land/controller/main.py
@@ -0,0 +1,28 @@
+from odoo import http
+from odoo.http import request
+from odoo.addons.web.controllers.main import serialize_exception, content_disposition
+import io
+try:
+    import base64
+except:
+    install('base64')
+
+try:
+    import xlsxwriter
+except:
+    install('xlsxwriter')
+
+import datetime
+
+class Binary(http.Controller):
+    @http.route('/web/binary/download_excell_report_schedule_land/<model("res.company"):company>', type='http', auth="public")
+    @serialize_exception
+    def download_document(self, company , **kw):
+        excel_data = self.env['report.schedule.land'].get_report_xls()
+
+        filename = f'Report_Cuotas_{company.name}.xlsx'
+        filecontent = base64.b64decode(excel_data or '')
+        return request.make_response(filecontent, [
+            ('Content-Type', 'application/vnd.ms-excel'),
+            ('Content-Disposition', content_disposition(filename))
+        ])
--- land/wizard/report_schedule_land.py
+++ land/wizard/report_schedule_land.py
@@ -1,4 +1,14 @@
 from odoo import api, fields, models
+import io
+try:
+    import base64
+except:
+    install('base64')
+
+try:
+    import xlsxwriter
+except:
+    install('xlsxwriter')
 
 class ReportScheduleLand(models.TransientModel):
     _name = "report.schedule.land"
@@ -6,4 +16,18 @@ class ReportScheduleLand(models.TransientModel):
     company_id = fields.Many2one('res.company', required=True, default=lambda self: self.env.company)
 
     def do_excell(self):
-        return
+        return {
+            'type': 'ir.actions.act_url',
+            'url': f'/web/binary/download_excell_report_schedule_land/{self.company_id.id}',
+            'target': 'self',
+        }
+
+    def get_report_xls(self):
+        fp = io.BytesIO()
+        workbook = xlsxwriter.Workbook(fp)
+        sheet = workbook.add_worksheet()
+        #self.get_report_xls_data(workbook, sheet)
+        workbook.close()
+        excel_file = base64.encodebytes(fp.getvalue())
+        fp.close()
+        return excel_file
