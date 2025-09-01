291c11d73673ff1d843abf2d3f781d5e2d5b9fb0
JESUS <you@example.com>
2025-08-31 21:19:31 -0500

    PARA PROBAR

--- land/controller/main.py
+++ land/controller/main.py
@@ -1,6 +1,9 @@
 from odoo import http
 from odoo.http import request
-from odoo.addons.web.controllers.main import serialize_exception, content_disposition
+#from odoo.addons.web.controllers.main import serialize_exception, content_disposition
+#from odoo.addons.web.controllers.main import  content_disposition
+#from odoo.http import content_disposition, Controller, request, route
+from odoo.http import content_disposition
 import io
 try:
     import base64
@@ -16,7 +19,7 @@ import datetime
 
 class Binary(http.Controller):
     @http.route('/web/binary/download_excell_report_schedule_land/<model("res.company"):company>', type='http', auth="public")
-    @serialize_exception
+    #@serialize_exception
     def download_document(self, company , **kw):
         excel_data = self.env['report.schedule.land'].get_report_xls()
 
