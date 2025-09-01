22935aebd4e9ec120581b52c8d445a00abe61233
JESUS <you@example.com>
2025-08-31 20:35:51 -0500

    UP

--- land/wizard/report_schedule_land.py
+++ land/wizard/report_schedule_land.py
@@ -4,3 +4,6 @@ class ReportScheduleLand(models.TransientModel):
     _name = "report.schedule.land"
     _description  = "report.schedule.land"
     company_id = fields.Many2one('res.company', required=True, default=lambda self: self.env.company)
+
+    def do_excell(self):
+        return
