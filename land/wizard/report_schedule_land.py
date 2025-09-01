5bc3100e51771ad8148dc71e094c6ff7f8be9b19
JESUS <you@example.com>
2025-08-31 22:29:14 -0500

    PARA PROBAR

--- land/wizard/report_schedule_land.py
+++ land/wizard/report_schedule_land.py
@@ -26,8 +26,20 @@ class ReportScheduleLand(models.TransientModel):
         fp = io.BytesIO()
         workbook = xlsxwriter.Workbook(fp)
         sheet = workbook.add_worksheet()
-        #self.get_report_xls_data(workbook, sheet)
+        self.get_report_xls_data(workbook, sheet)
         workbook.close()
         excel_file = base64.encodebytes(fp.getvalue())
         fp.close()
         return excel_file
+
+    def get_report_xls_data(self,workbook, sheet):
+        bold = workbook.add_format({'bold': True})
+        sheet.write(xc, 1, 'A', bold)
+        sheet.write(xc, 2, 'B', bold)
+        #sheet.write(xc, 3, 'UM', bold)
+        #sheet.write(xc, 4, _('Unit Cost'), bold)
+        #sheet.write(xc, 5, _('Total Cost'), bold)
+        #sheet.write(xc, 6, _('Price Unit'), bold)
+        #sheet.write(xc, 7, _('Price Total'), bold)
+
+
