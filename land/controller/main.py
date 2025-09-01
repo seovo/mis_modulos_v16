90f0f1716b4160bc00d9611322cb61ffff6be51b
JESUS <you@example.com>
2025-08-31 21:21:15 -0500

    PARA PROBAR

--- land/controller/main.py
+++ land/controller/main.py
@@ -21,7 +21,7 @@ class Binary(http.Controller):
     @http.route('/web/binary/download_excell_report_schedule_land/<model("res.company"):company>', type='http', auth="public")
     #@serialize_exception
     def download_document(self, company , **kw):
-        excel_data = self.env['report.schedule.land'].get_report_xls()
+        excel_data = request.env['report.schedule.land'].get_report_xls()
 
         filename = f'Report_Cuotas_{company.name}.xlsx'
         filecontent = base64.b64decode(excel_data or '')
