c6bc84429b9ee556251b9f2048a87ed13965893c
JESUS <you@example.com>
2025-09-08 15:41:59 -0500

    UP

--- l10n_pe_edi_extens_jz/account_move.py
+++ l10n_pe_edi_extens_jz/account_move.py
@@ -3,16 +3,21 @@ from odoo import _, api, fields, models
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
+    @api.onchange('invoice_date')
+    def change_date_jz_jz(self):
+        for record in self:
+            if record.invoice_date:
+                diff = fields.Datetime.now().date() - record.invoice_date
+
+                raise ValueError(diff.date)
+
 
     def action_post(self):
-
         res = super().action_post()
-
         if len(self) == 1 :
             if self.edi_document_ids:
                 self.button_process_edi_web_services()
-
-        return  res
+        return res
 
 
 
@@ -32,7 +37,8 @@ class AccountMove(models.Model):
 
         res = super().button_process_edi_web_services()
 
-        if self.l10n_pe_edi_operation_type == '1001':
+        #if self.l10n_pe_edi_operation_type == '1001':
+        if len(self) == 1:
             self.narration = note
 
 
