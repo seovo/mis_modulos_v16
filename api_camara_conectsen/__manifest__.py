249a1d206635f9f3d2a53ead6a4d0dd4456c397b
JESUS <you@example.com>
2025-10-21 20:06:36 -0500

    up

---
+++ api_camara_conectsen/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Api Conectsen",
+    "summary": "Api Conectsen",
+    "description": """Api Conectsen""",
+    "author": "Jzolutions",
+    "category": "Uncategorized",
+    "version": "17.0",
+    "depends": ["website_sale"],
+    "data": [
+        #"security/ir.model.access.csv",
+        #'views/view.xml'
+    ],
+    # 'uninstall_hook': 'uninstall_hook',
+    #"external_dependencies": {"python": ["msal"]},
+    "application": True,
+    "installable": True,
+    "auto_install": False,
+
+}
---
+++ api_camara_conectsen/controller/main.py
@@ -0,0 +1,72 @@
+import logging
+import pprint
+from odoo import http
+from odoo.http import request
+from odoo import _, fields, models
+from datetime import datetime, timedelta
+
+from odoo.exceptions import AccessError, MissingError, ValidationError
+from odoo.tools import lazy, str2bool
+from odoo.addons.payment.controllers.post_processing import PaymentPostProcessing
+
+_logger = logging.getLogger(__name__)
+from odoo.exceptions import AccessError
+import odoo
+import uuid
+from odoo.addons.payment.controllers import portal as payment_portal
+
+from odoo.addons.website.models.ir_http import sitemap_qs2dom
+from werkzeug.exceptions import Forbidden, NotFound
+from odoo.addons.http_routing.models.ir_http import slug
+from odoo.addons.website.controllers.main import QueryURL
+from odoo.osv import expression
+from odoo.addons.website_sale.controllers.main import TableCompute
+from odoo.addons.website_sale.controllers.main import WebsiteSale
+from odoo.tools import lazy, str2bool
+from odoo.tools.json import scriptsafe as json_scriptsafe
+from odoo import fields, http, SUPERUSER_ID, tools, _
+
+
+
+
+class ApiClinicos(http.Controller):
+
+
+
+    @http.route(['/api/v1/detect/binarys'], type='json', auth="public", methods=['POST'],
+                website=True, csrf=False)
+    def apiclinicos_signup(self, **post):
+        #db = http.request.env.cr.dbname
+        data = http.request.httprequest.get_json()
+
+        return data
+
+        #values = {key: qcontext.get(key) for key in ('login', 'name', 'password')}
+
+        #partner = request.env['res.partner'].sudo().create({
+        #    'name':
+        #})
+
+        values = {
+            'name': data['name'] ,
+            'email':  data['email'] ,
+            'login': data['email'],
+            'password': data['password'] ,
+            'lang': 'es_AR'
+        }
+
+        #if not values:
+        #    raise UserError(_("The form was not properly filled in."))
+
+        ########
+
+        login, password = request.env['res.users'].sudo().signup(values)
+        request.env.cr.commit()  # as authenticate will use its own cursor we need to commit the current transaction
+        pre_uid = request.session.authenticate(request.db, login, password)
+        if not pre_uid:
+            return False
+            #raise SignupError(_('Authentication Failed.'))
+
+        return True
+
+
