013c5a6e5767073fdbba038336ea96d95937ed2c
JESUS <you@example.com>
2025-10-26 18:25:50 -0500

    OKA

---
+++ iso_qsg/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{   "name": "ISO QSG",
+    "summary": """ISO QSG""",
+    "description": """ISO QSG""",
+    "author": "JZolutions",
+    "category": "Uncategorized",
+    "version": "1.0",
+    "license": "OPL-1",
+    "depends": [
+        "multiversion_iso_qsg",
+        #"stock",
+        #"website_sale",
+        #"contacts",
+        #"sale_management",
+
+    ],
+    # always loaded
+    "data": [
+        'views/process_qsg.xml',
+        'views/menu.xml'
+
+    ],
+    # 'images': ['static/description/odoo-woo.gif'],
+}
