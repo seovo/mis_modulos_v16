6f749b33f5674faba68e82a2c9a5b157146f0936
JESUS <you@example.com>
2025-11-01 20:55:07 -0500

    OKA

--- migrate_version_odoo_jz/models/__init__.py
+++ migrate_version_odoo_jz/models/__init__.py
@@ -1,4 +1,5 @@
-
 from . import migrate_jz
+from . import migrate_model_fields
+from . import migrate_model_jz
 
 
---
+++ migrate_version_odoo_jz/models/migrate_model_jz.py
@@ -0,0 +1,327 @@
+from odoo import api, fields, models , _
+#from odoo.tools import float_is_zero, format_amount, format_date, html_keep_url, is_html_empty
+from dateutil.relativedelta import relativedelta
+from datetime import datetime, timedelta
+from odoo.exceptions import ValidationError
+
+import psycopg2
+from psycopg2 import sql
+
+class MigrateModelJz(models.Model):
+    _name = 'migrate.model.jz'
+    model_id = fields.Many2one('ir.model',string="Modelo")
+    table = fields.Char(required=True)
+    new_table = fields.Char(string="Nueva Tabla")
+    columns = fields.One2many('migrate.model.columns.jz','migrate_model_id')
+    log = fields.Text()
+    migrate_id = fields.Many2one('migrate.jz')
+    name = fields.Char(related='table')
+    update_if_exist = fields.Boolean(string="Actualizar si Existe")
+    ignorar_if_error = fields.Boolean(string="Ignorar si Error")
+    no_existe_id = fields.Boolean()
+    where_set = fields.Text()
+
+
+    @api.onchange('model_id')
+    def change_model(self):
+        for record in self:
+            if record.model_id:
+                table = record.model_id.model.replace('.','_')
+                #raise ValueError(table)
+                record.table = table
+
+    @api.onchange('table')
+    def change_table(self):
+        table = self.table
+        cursor = self.migrate_id.conect_postgres()
+
+        string_sql = f"SELECT * FROM {table} LIMIT 1"
+        try:
+            cursor.execute(string_sql)
+
+        except:
+            #cursor.execute(string_sql)
+            self.columns = None
+            return
+
+        self.columns = None
+        for desc in cursor.description:
+            #if desc[0] == 'name':
+            #    raise ValueError(desc[1])
+            dx = {
+                'name': desc[0]
+            }
+            if desc[1] == 3802 :
+                dx.update({'type_field':'jsonb_text'})
+
+            #para version16 a version 17
+            if table in ['product_template']:
+                if desc[0] == 'message_main_attachment_id':
+                    dx.update({'ignore': True})
+
+            if table in ['product_product']:
+                if desc[0] == 'message_main_attachment_id':
+                    dx.update({'ignore': True})
+
+            if table in ['res_partner']:
+                if desc[0] == 'display_name':
+                    dx.update({
+                        'name': 'complete_name',
+                        'value_set': 'display_name as complete_name'
+                    })
+
+                if desc[0] == 'message_main_attachment_id':
+                    dx.update({'ignore': True})
+
+
+            self.columns += self.env['migrate.model.columns.jz'].new(dx)
+
+    def migrate_table(self):
+
+        case_sql = None
+
+
+
+        cursor = self.migrate_id.conect_postgres()
+
+        select_columnsx = []
+        column_names = []
+
+        for colx in self.columns:
+            #raise ValueError([col,col.ignore])
+            if colx.ignore == True:
+                continue
+                #continue
+
+            namm = f'"{colx.name}"'
+
+
+
+            column_names.append(namm)
+
+            if colx.type_field in ['text_jsonb']:
+                #namm += '::jsonb'
+                namm = f'''
+                jsonb_build_object(
+                    'en_US', {colx.name},
+                    'es_PE', {colx.name}
+                )::text AS {namm}
+                '''
+
+
+            if colx.type_field in ['jsonb_text']:
+                namm += '::text'
+                #namm = f''' '"' || jsonb_to_json({namm}) || '"' AS {namm}_json '''
+            if colx.value_set :
+                namm = f'''{colx.value_set} '''
+
+            if colx.is_field:
+                if not case_sql:
+
+                    insert_case =  ''
+                    for line in self.migrate_id.field_ids:
+                        if line.ir_model_fields_id:
+                            insert_case += f''' WHEN {line.id_sql} IS NULL   THEN {line.ir_model_fields_id.id}  '''
+
+
+                    case_sql = f'''
+                    CASE
+                    {insert_case}
+                    END AS field
+                    '''
+
+                namm = case_sql
+
+            select_columnsx.append(namm)
+            if colx.ignore :
+                raise ValueError([colx,colx.ignore,colx.name])
+
+
+        #raise ValueError(select_columnsx)
+
+        self._migrate_table(cursor, select_columnsx,column_names)
+
+
+
+    def _migrate_table(self,cursor,select_columns,column_names):
+        table = self.table
+        #raise ValueError(select_columns)
+        #select_columns = [f'"{element}"' for element in select_columns]
+        string_columns = ",".join(select_columns)
+        #quitar limit
+        string_sql = f"SELECT {string_columns} FROM {table} "
+        if table == 'res_users':
+            add_where = f' AND {self.where_set}   ' if self.where_set else ''
+            string_sql += f'  where id != {self.env.user.id} {add_where} ;'
+        else:
+            if self.where_set:
+                string_sql += f'  where {self.where_set} ;'
+
+
+        #raise ValueError(string_sql)
+
+        cursor.execute(string_sql)
+
+        #try:
+        #    cursor.execute(string_sql)
+        #except:
+        #    raise ValueError(string_sql)
+
+
+        if self.table == 'product_attribute_value_product_product_rel' and self.migrate_id.from_version == 12:
+
+            self.insert_product_variant_combination( cursor, table, column_names)
+
+
+        else:
+            self.insert_record_migrate(cursor, table, column_names)
+
+
+
+        #resultados = cursor.fetchall()
+
+    def insert_product_variant_combination(self, cursor, table, column_names):
+        resultados = cursor.fetchall()
+
+        insert_sql = ''
+
+        for fila in resultados:
+
+            product = self.env['product.product'].browse(fila[0])
+
+            sql = f'''
+            SELECT id , attribute_line_id
+                FROM product_template_attribute_value 
+                WHERE product_attribute_value_id = {fila[1]}
+                AND  product_tmpl_id  = {product.product_tmpl_id.id}
+               
+            '''
+
+
+            self.env.cr.execute(sql)
+            data =  self._cr.fetchall()
+            if data:
+
+                SQL_INSERT = f'''
+                       INSERT INTO product_variant_combination(product_product_id,product_template_attribute_value_id)
+                       VALUES ({fila[0]},{data[0][0]}) ON CONFLICT (product_product_id,product_template_attribute_value_id) DO NOTHING ; 
+                '''
+                self.env.cr.execute(SQL_INSERT)
+                #SQL_INSERT = f'''
+                #    INSERT INTO product_attribute_value_product_template_attribute_line_rel(product_attribute_value_id,product_template_attribute_line_id)
+                #    VALUES ({fila[1]},{data[0][1]}) ON CONFLICT (product_attribute_value_id,product_template_attribute_line_id) DO NOTHING ;
+                #'''
+
+                #self.env.cr.execute(SQL_INSERT)
+
+                #product_attribute_value_id
+                #product_template_attribute_line_id
+
+
+                #raise ValueError([data, sql])
+
+
+    def insert_record_migrate(self,cursor,table,column_names):
+
+        if self.new_table:
+            table = self.new_table
+
+        #column_names = [f'"{element}"' for element in column_names]
+
+        resultados = cursor.fetchall()  # Obtener todos los resultados
+        #raise ValueError(resultados)
+
+        #raise ValueError(column_names)
+
+        n = len(column_names)  # Cambia este valor a la cantidad de {} que deseas
+        corchetes_n = ','.join('%s' for _ in range(n))
+
+        # Generar la instrucción INSERT
+        #raise ValueError(resultados)
+        for fila in resultados:
+            val1 = ','.join(column_names)
+            val2 = corchetes_n
+            # raise ValueError(val3)
+
+            if self.no_existe_id:
+                if self.ignorar_if_error:
+                    conflict = val1.replace('"','')
+                    #SQL_INSERT = f"INSERT INTO {table} ({val1}) VALUES ({val2})  ON CONFLICT ({conflict}) DO NOTHING  "
+                    SQL_INSERT = f'''
+                    DO $$
+BEGIN
+    INSERT INTO {table} ({val1}) VALUES ({val2})  ON CONFLICT ({conflict}) DO NOTHING ;
+    
+EXCEPTION
+    WHEN foreign_key_violation THEN
+        RAISE NOTICE 'Error: El registro de invoice_line_id no existe. Ignorando...';
+    WHEN others THEN
+        RAISE NOTICE 'Se produjo un error inesperado. Ignorando...';
+        
+END $$;
+                    '''
+                else:
+                    SQL_INSERT = f"INSERT INTO {table} ({val1}) VALUES ({val2}) "
+
+            else:
+                if self.update_if_exist:
+                    val3 = ','.join(
+                        "{} = EXCLUDED.{}".format(col, col) for col in column_names
+                        if col != 'id'
+                    )
+
+                    SQL_INSERT = f"INSERT INTO {table} ({val1}) VALUES ({val2}) ON CONFLICT (id) DO UPDATE SET {val3}"
+                else:
+                    SQL_INSERT = f"INSERT INTO {table} ({val1}) VALUES ({val2}) ON CONFLICT (id) DO NOTHING"
+
+                if self.ignorar_if_error:
+                    SQL_INSERT = f'''
+                                        DO $$
+                    BEGIN
+                        {SQL_INSERT}  ; 
+
+                    EXCEPTION
+                        WHEN foreign_key_violation THEN
+                            RAISE NOTICE 'Error: El registro de invoice_line_id no existe. Ignorando...';
+                        WHEN others THEN
+                            RAISE NOTICE 'Se produjo un error inesperado. Ignorando...';
+
+                    END $$; '''
+
+
+
+            #raise ValueError([SQL_INSERT,fila])
+            self.env.cr.execute(SQL_INSERT, fila)
+
+
+            '''
+            try:
+                self.env.cr.execute(SQL_INSERT, fila)
+            except:
+                raise ValueError([SQL_INSERT, fila])
+                #raise ValueError([fila,SQL_INSERT])
+                sql_strr = "SELECT * FROM product_template  "
+                self.env.cr.execute(sql_strr)
+                result = self.env.cr.fetchall()
+                raise ValueError(result)
+                raise ValueError([SQL_INSERT,fila,result])
+            '''
+
+
+
+
+
+            # raise ValueError(SQL_INSERT)
+
+            # insert_query = sql.SQL(SQL_INSERT)
+            # Ejecutar la instrucción
+            # raise ValueError(SQL_INSERT)
+            # cursor.execute(SQL_INSERT, fila)
+
+
+
+        if not self.no_existe_id:
+            sql_increment = f''' SELECT setval('public.{table}_id_seq', MAX(id)) FROM {table};'''
+            self.env.cr.execute(sql_increment)
+
+
