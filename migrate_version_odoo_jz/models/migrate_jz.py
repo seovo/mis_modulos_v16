81c2a275ed28a12ebe9e10c3d212b76f5a662258
Your Name <you@example.com>
2025-12-06 12:16:35 -0500

    UP

--- migrate_version_odoo_jz/models/migrate_jz.py
+++ migrate_version_odoo_jz/models/migrate_jz.py
@@ -382,7 +382,7 @@ class MigrateJz(models.Model):
                 try:
                     mvl._compute_totals()
                 except:
-                    raise ValidationError(mvl.move_id)
+                    #raise ValidationError(mvl.move_id)
                     continue
 
             return
