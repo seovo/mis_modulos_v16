718ff1ee52c99621d6a62a60c635f4c08508bf7d
Your Name <you@example.com>
2025-12-26 23:41:58 -0500

    update

--- migrate_version_odoo_jz/models/migrate_jz.py
+++ migrate_version_odoo_jz/models/migrate_jz.py
@@ -437,9 +437,11 @@ class MigrateJz(models.Model):
 
         moves_without_payment = self.env['account.move'].search([
             ('status_in_payment', '=', 'not_paid'),
+            ('amount', '=', 'not_paid'),
             ('state', '=', 'posted'),
             ('move_type', 'in', ['out_invoice', 'out_refund']),
             ('move_type', '!=', 'entry')], limit=20)
+
 
         raise ValidationError(moves_without_payment)
 
