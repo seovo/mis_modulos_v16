df5d3654c1a12ae8c5a688233944b0d8c75a6404
Your Name <you@example.com>
2025-12-27 00:10:02 -0500

    update

--- migrate_version_odoo_jz/models/migrate_jz.py
+++ migrate_version_odoo_jz/models/migrate_jz.py
@@ -443,7 +443,7 @@ class MigrateJz(models.Model):
             ("matched_payment_ids", "!=", False)], limit=1)
 
 
-        #raise ValidationError(moves_without_payment)
+        raise ValidationError(moves_without_payment)
 
         if moves_without_payment:
             for paymentm in moves_without_payment:
