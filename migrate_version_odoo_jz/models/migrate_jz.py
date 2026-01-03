3f577c7a59e261317e853dd7c0ab402bb5811495
Your Name <you@example.com>
2026-01-02 19:57:44 -0500

    update

--- migrate_version_odoo_jz/models/migrate_jz.py
+++ migrate_version_odoo_jz/models/migrate_jz.py
@@ -429,22 +429,7 @@ class MigrateJz(models.Model):
             cron_models.migrate_table()
             return
 
-        moves_without_binary = self.env['account.move'].search([
-            ('invoice_payments_widget', '=', False),
-            ('move_type', '!=', 'entry'),
-            ('invoice_line_ids', '!=', False)], limit=1500)
-        if moves_without_binary:
-            for mw in moves_without_binary:
 
-                try:
-
-                    mw._compute_payments_widget_reconciled_info()
-
-                except:
-                    continue
-                    raise ValidationError(mw)
-
-            return
 
         moves_without_partner = self.env['account.move'].search([
             ('partner_id','!=',False),
