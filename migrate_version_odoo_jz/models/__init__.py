dbee6b2c06f753656db523ab07413ad02437b44f
Your Name <you@example.com>
2026-01-02 23:32:28 -0500

    update

--- migrate_version_odoo_jz/models/__init__.py
+++ migrate_version_odoo_jz/models/__init__.py
@@ -2,5 +2,5 @@ from . import migrate_jz
 from . import migrate_model_fields
 from . import migrate_model_jz
 from . import journal_migration_jz
-#from . import account_move
+from . import account_move
 
