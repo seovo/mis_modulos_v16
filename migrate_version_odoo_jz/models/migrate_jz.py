6f6a13c7dd288f68a00a6f4ff6802b64e617f30b
Your Name <you@example.com>
2026-01-07 11:04:51 -0500

    NEW FIX

--- migrate_version_odoo_jz/models/migrate_jz.py
+++ migrate_version_odoo_jz/models/migrate_jz.py
@@ -472,7 +472,7 @@ class MigrateJz(models.Model):
 
                     #raise ValidationError(tt)
                     sql = f'''UPDATE account_move_line SET amount_currency = %s  WHERE id = %s'''
-                    raise  ValidationError([tt,'-----',line.id])
+                    raise  ValidationError([line.balance,line.currency_rate,tt,'-----',line.id])
                     self.env.cr.execute(sql,[tt,line.id])
                     #line.amount_currency = tt
 
