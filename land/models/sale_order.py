052ce895ab2aae46dcc368b5fa936b865ad17252
Your Name <you@example.com>
2026-01-08 07:29:58 -0500

    UPDATE

--- land/models/sale_order.py
+++ land/models/sale_order.py
@@ -731,6 +731,7 @@ class SaleOrder(models.Model):
             record.qty_dues_independence_payment = qty_dues_independence_payment
             record.diff_dues_independence = total_dues_independence - qty_dues_independence_payment
 
+
     @api.onchange('date_sign_land','type_periodo_invoiced')
     @api.depends('date_sign_land', 'type_periodo_invoiced')
     def change_date_first_due_date(self):
