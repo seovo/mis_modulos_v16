bbd4dd980f4557a26bce391e359ee4930796468f
Your Name <you@example.com>
2026-01-08 23:32:36 -0500

    FIN MIGRACION V11 A V18

--- l10n_pe_edi_extens_jz/account_move.py
+++ l10n_pe_edi_extens_jz/account_move.py
@@ -36,9 +36,17 @@ class AccountMove(models.Model):
 
 
 
+
+
+
     def action_retry_edi_documents_error(self):
 
         self.validate_cpe()
+
+        for record in self:
+            if record.state == 'to_send':
+                record.button_draft()
+                record.action_post()
 
         res = super(AccountMove, self).action_retry_edi_documents_error()
         return res
