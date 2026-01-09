362500301a61acae8a0370c0cd4a03f03dc65403
Your Name <you@example.com>
2026-01-09 09:36:18 -0500

    FIN MIGRACION V11 A V18

--- l10n_pe_edi_extens_jz/account_move.py
+++ l10n_pe_edi_extens_jz/account_move.py
@@ -49,7 +49,7 @@ class AccountMove(models.Model):
         moves = self.env['account.move'].search([('edi_state','=','to_send'),
                                                  ('attempts_validate_sunat_jz','=',0)],limit=1)
 
-        raise ValidationError(moves)
+        #raise ValidationError(moves)
 
         moves.action_retry_edi_documents_error()
 
