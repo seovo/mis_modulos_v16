f86765bd94929b016a31f6ee551117fc64f28f21
Your Name <you@example.com>
2026-01-08 23:40:54 -0500

    FIN MIGRACION V11 A V18

--- l10n_pe_edi_extens_jz/account_move.py
+++ l10n_pe_edi_extens_jz/account_move.py
@@ -41,6 +41,7 @@ class AccountMove(models.Model):
 
     def action_retry_edi_documents_error(self):
 
+        raise ValidationError('HOLA')
         #self.validate_cpe()
 
         if len(self) == 1:
