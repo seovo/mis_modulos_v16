2bd5428d8d0e79f2192f541624e9dc43a2b3212b
Your Name <you@example.com>
2026-01-19 12:32:14 -0500

    UP

--- datawave/models/__init__.py
+++ datawave/models/__init__.py
@@ -1,3 +1,4 @@
 from . import inputs_cliente
 from . import inputs_transaccional
 from . import input_others
+from . import intermedios
---
+++ datawave/models/intermedios.py
@@ -0,0 +1,66 @@
+from odoo import models, fields
+
+class SigmaCD(models.Model):
+    _name = 'datawave.sigma.cd'
+    _description = 'SigmaCD de Datawave'
+    name = fields.Char(string='Clave')
+    sku = fields.Char(string='Producto_ID')
+    code_cd = fields.Char(string='CD_ID')
+    sigma  = fields.Float(string='Sigma')
+
+class SigmaTienda(models.Model):
+    _name = 'datawave.sigma.tienda'
+    _description = 'SigmaTienda de Datawave'
+    name = fields.Char(string='Clave')
+    sku = fields.Char(string='Producto_ID')
+    code_tienda = fields.Char(string='Tienda_ID')
+    sigma = fields.Float(string='Sigma')
+
+
+class IntermedioTienda(models.Model):
+    _name = 'datawave.intermedio.tienda'
+    _description = 'IntermedioTienda de Datawave'
+    name         = fields.Char(string='Clave')
+    sku          = fields.Char(string='Producto_ID')
+    code_tienda  = fields.Char(string='Tienda_ID')
+    lt_days      = fields.Integer(string='LT Dias')
+    forecast_day = fields.Integer(string='Forecast Diario')
+    sigma        = fields.Float(string='Sigma')
+    ss           = fields.Float(string='SS',help='Z*Sigma*SQRT(LT)')
+    freq         = fields.Float(string='FREQ')
+    max          = fields.Float(string='MAX',help='(LT+FREQ)*Forecast+SS')
+    rop          = fields.Float(string='ROP',help='Forecast*LT+SS')
+    stock        = fields.Integer(string='Stock')
+    quantity     = fields.Integer(string='Cantidad Sugerida',help='MAX(0,MAX-Stock)')
+    quantity_round = fields.Integer(string='Cantidad Sugerida Redondeada')
+
+class IntermedioCD(models.Model):
+    _name = 'datawave.intermedio.cd'
+    _description = 'IntermedioCD de Datawave'
+    name         = fields.Char(string='Clave')
+    code         = fields.Char(string='Clave2')
+    sku          = fields.Char(string='Producto_ID')
+    code_cd      = fields.Char(string='CD_ID')
+    code_seller  = fields.Char(string='Proveedor ID')
+    lt_days      = fields.Integer(string='LT Dias')
+    forecast_day = fields.Integer(string='Forecast Diario')
+    sigma        = fields.Float(string='Sigma')
+    moq          = fields.Float(string='Sigma')
+    stock        = fields.Integer(string='Stock')
+    stock_transit = fields.Integer(string='Stock Transito')
+    stock_forecast = fields.Integer(string='Stock Pronosticado')
+    ss = fields.Float(string='SS', help='Z*Sigma*SQRT(LT)')
+    freq = fields.Float(string='FREQ')
+    max = fields.Float(string='MAX', help='(LT+FREQ)*Forecast+SS')
+    rop = fields.Float(string='ROP', help='Forecast*LT+SS')
+    quantity = fields.Integer(string='Cantidad Sugerida', help='MAX(0,MAX-Stock)')
+    quantity_round = fields.Integer(string='Cantidad Sugerida Redondeada')
+
+
+
+
+
+
+
+
+
