c46202be23b665fdd2522b0b0e35c70de7fb4ff8
Your Name <you@example.com>
2026-01-23 01:35:50 -0500

    up

--- datawave/models/res_config_settings.py
+++ datawave/models/res_config_settings.py
@@ -7,4 +7,5 @@ class ResConfigSettings(models.TransientModel):
     datawave_sigma_days = fields.Integer('Días usados para Sigma tienda',config_parameter='datawave.ventana_sigma_dias')
     datawave_z_tienda   = fields.Float('Z por tienda (fallback)',config_parameter='datawave.z_tienda')
     datawave_metodo_frecuencia_tienda = fields.Selection([('1','Fija'),('2','LT+Delta'),('3','Días objetivo')],
-                                                         default=1,config_parameter='datawave.metodo_frecuencia_tienda')
+                                                         default='1',
+                                                         config_parameter='datawave.metodo_frecuencia_tienda')
