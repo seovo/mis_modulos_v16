7fa70a03a8b1a3c282952b97df55939ea589e6ca
Your Name <you@example.com>
2026-01-24 06:33:56 -0500

    up

--- datawave/models/res_config_settings.py
+++ datawave/models/res_config_settings.py
@@ -4,6 +4,9 @@ from odoo import fields, models
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
+    datawave_ss_tienda = fields.Integer('Método stock de seguridad tienda',
+                                         config_parameter='datawave.metodo_ss_tienda')
+
     datawave_sigma_days = fields.Integer('Días usados para Sigma tienda',config_parameter='datawave.ventana_sigma_dias')
     datawave_z_tienda   = fields.Float('Z por tienda (fallback)',config_parameter='datawave.z_tienda')
     datawave_metodo_frecuencia_tienda = fields.Selection([('1','Fija'),('2','LT+Delta'),('3','Días objetivo')],
