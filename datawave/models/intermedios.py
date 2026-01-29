804ac8c141118226cb15f248729a272f892d589e
Your Name <you@example.com>
2026-01-29 10:51:25 -0500

    PARA MIGRAGR

--- datawave/models/intermedios.py
+++ datawave/models/intermedios.py
@@ -30,10 +30,17 @@ class IntermedioTienda(models.Model):
         ('0', 'Sin SobreStock'), ('1', 'SobreStocko 1'), ('2', 'SobreStock 2')
     ])
 
+    _sql_constraints = [
+        (
+            "unique_intermedio_tienda",
+            "unique(date, product_id , tienda_id)",
+            "NO PUEDE HABER REGISTROS DUPLICADOS",
+        )
+    ]
+
+    #ESTABLECER EL FORECAST
     def set_forecast_day(self,forecast_tienda_day):
         record = self
-
-
         if forecast_tienda_day:
             record.forecast_tienda_id = forecast_tienda_day.id
             record.forecast_day = forecast_tienda_day.forecast_day
@@ -41,18 +48,13 @@ class IntermedioTienda(models.Model):
             record.forecast_tienda_id = None
             record.forecast_day = None
 
-    def set_historico_sigma(self,config_tienda):
+    def set_historico_sigma(self):
 
         record = self
 
-        config = config_tienda
 
-        # raise ValueError(config)
 
-        if not config:
-            return
 
-        record.lt_days = config.lt_days
         sigma_dias = self.env['ir.config_parameter'].sudo().get_param('datawave.ventana_sigma_dias')
         sigma_dias = int(sigma_dias) if sigma_dias else 0
 
@@ -88,25 +90,23 @@ class IntermedioTienda(models.Model):
         #    raise ValueError(domain)
 
 
-    def set_zz(self):
-        conf_zz = self.env['ir.config_parameter'].sudo().get_param('datawave.z_tienda')
-        conf_zz = float(conf_zz) if conf_zz else 0
+    #FUNCION STOCK SEGURIDAD
+    def set_stock_seguridad(self):
+        conf_ss = self.env['ir.config_parameter'].sudo().get_param('datawave.z_tienda')
+        conf_ss = float(conf_ss) if conf_ss else 0
 
-        if conf_zz == 1:
+        if conf_ss == 1:
             import math
 
             # Calcular la raíz cuadrada
             raiz_cuadrada = math.sqrt(self.lt_days)
-            zz = conf_zz * self.sigma * raiz_cuadrada
-            self.ss = zz
+            ss = conf_ss * self.sigma * raiz_cuadrada
+            self.ss = ss
         else:
             self.ss = self.lt_days * self.forecast_day
 
-    def set_frecuency(self,forecast_tienda_day):
-        conf = self.env['ir.config_parameter'].sudo().get_param('datawave.metodo_frecuencia_tienda')
-        conf = int(conf) if conf else 0
-
-        self.freq = 0
+    #FUNCION FRECUENCIA
+    def set_frecuency(self,conf,forecast_tienda_day):
 
         config_tienda = self.env['datawave.config.tienda'].search([
             ('tienda_id', '=', self.tienda_id.id)
@@ -125,11 +125,7 @@ class IntermedioTienda(models.Model):
         if conf == 3:
             self.freq = forecast_tienda_day.days_target
 
-    def set_max(self):
-        self.max = 0
-
-        conf = self.env['ir.config_parameter'].sudo().get_param('datawave.metodo_max_tienda')
-        conf = int(conf) if conf else 0
+    def set_max(self,conf):
 
         if conf == 1 :
             self.max = ( ( self.lt_days + self.freq ) * self.freq ) + self.ss
@@ -138,21 +134,9 @@ class IntermedioTienda(models.Model):
             self.max = (14 * self.forecast_day ) + self.ss
 
 
-    def set_stock(self):
-        stock = 0
-        domain = [
-            ('tienda_id', '=', self.tienda_id.id),
-            ('product_id', '=', self.product_id.id),
-            ('date', '=', self.date)
-        ]
-        stock_tienda = self.env['datawave.stock.tienda'].search(domain)
+    def set_stock_riesgo_sobrestock(self,stock_tienda):
 
-        if stock_tienda:
-            stock = stock_tienda.stock
-        # else:
-        #    raise ValueError(domain)
-
-        self.stock = stock
+        self.stock = stock if stock_tienda else 0
         self.quantity = max(self.stock, self.max)
 
         if self.stock <= 0:
@@ -175,52 +159,76 @@ class IntermedioTienda(models.Model):
             else:
                 self.sobreestock = '0'
 
+    def quantity_reponer(self, config_tienda_p):
+        def multiplo_superior(numero, constante):
+            if constante <= 0:
+                raise ValueError("La constante debe ser mayor que 0.")
+            return ((numero + constante - 1) // constante) * constante
 
-
-
-
-
+        resultado = multiplo_superior(self.quantity, config_tienda_p.round_tienda)
+        self.quantity_round = resultado
 
 
     @api.onchange('product_id','tienda_id','date')
     def change_product_tienda(self):
+
+        conf_frecuencia_tienda = self.env['ir.config_parameter'].sudo().get_param('datawave.metodo_frecuencia_tienda')
+        conf_frecuencia_tienda = int(conf_frecuencia_tienda) if conf_frecuencia_tienda else 0
+
+        conf_max = self.env['ir.config_parameter'].sudo().get_param('datawave.metodo_max_tienda')
+        conf_max = int(conf_max) if conf_max else 0
+
+
+
         for record in self:
+
+            #INICIALIAR VARIABLES
             record.lt_days = 0
             record.datawave_sale_ids = False
+            record.forecast_tienda_id = False
+            record.lt_days = 0
+            record.ss = 0
+            record.freq = 0
+            record.max = 0
+            record.stock = 0
+            record.quantity = 0
+            record.riesgo = False
+            record.sobreestock = False
 
             if not record.product_id or not record.tienda_id  or not record.date:
                 continue
 
+
+
+            #OBTENER LA CONFIGURACION DE LA TABLA CONFI_TIENDA_PRODUCTO
             config_tienda_p = self.env['datawave.config.tienda.product'].search([
                 ('product_id', '=', record.product_id.id), ('tienda_id', '=', record.tienda_id.id)
             ])
 
+
+            #OBTENER EL FORECAST
             forecast_tienda_day = self.env['datawave.forecast.tienda'].search([
                 ('product_id', '=', record.product_id.id), ('tienda_id', '=', record.tienda_id.id)
             ])
 
+            #stock_tienda
+            domain = [
+                ('tienda_id', '=', record.tienda_id.id),
+                ('product_id', '=', record.product_id.id),
+                ('date', '=', record.date)
+            ]
+            stock_tienda = self.env['datawave.stock.tienda'].search(domain)
+
+            record.lt_days = config_tienda_p.lt_days if config_tienda_p else 0
             record.set_forecast_day(forecast_tienda_day)
-            record.set_historico_sigma(config_tienda_p)
-            record.set_zz()
-            record.set_frecuency(forecast_tienda_day)
-            record.set_max()
-
+            record.set_historico_sigma()
+            record.set_stock_seguridad()
+            record.set_frecuency(conf_frecuencia_tienda,forecast_tienda_day)
+            record.set_max(conf_max)
             record.rop = ( record.forecast_day * record.lt_days ) + record.ss
+            record.set_stock_riesgo_sobrestock(stock_tienda)
+            record.quantity_reponer(config_tienda_p)
 
-
-            record.set_stock()
-
-
-
-            def multiplo_superior(numero, constante):
-                if constante <= 0:
-                    raise ValueError("La constante debe ser mayor que 0.")
-                return ((numero + constante - 1) // constante) * constante
-
-
-
-            resultado = multiplo_superior(record.quantity, config_tienda_p.round_tienda)
-            record.quantity_round = resultado
 
 
 
