5d80269e303bb6343d612b0a39fd7abdf66d4daa
Your Name <you@example.com>
2026-04-03 01:00:22 -0500

    clean

--- smc_connector_jz/models/account_move.py
+++ smc_connector_jz/models/account_move.py
@@ -548,6 +548,8 @@ class AccountMove(models.Model):
                 bot_partner_id = bot_user.partner_id.id if bot_user else False
                 author_partner_id = bot_partner_id
 
+                ValueError(author_partner_id)
+
                 channel.sudo().message_post(
                     body=body,
                     subject=subject,
