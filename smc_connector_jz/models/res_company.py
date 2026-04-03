42522c701c90ab66c6006283ac238d2498ab2c15
Your Name <you@example.com>
2026-04-03 12:53:38 -0500

    clean

--- smc_connector_jz/models/res_company.py
+++ smc_connector_jz/models/res_company.py
@@ -11,6 +11,7 @@ class ResCompany(models.Model):
     smc_active = fields.Boolean()
     smc_journal_ids = fields.Many2many('account.journal','smc_journal_ids',string="Diarios Permitidos")
     smc_date_after = fields.Date(string="Despues de")
+
     smc_channel_id =  fields.Many2one('mail.channel', string='Canal Anuncios')
     #mail.thread
     #mail.channel
