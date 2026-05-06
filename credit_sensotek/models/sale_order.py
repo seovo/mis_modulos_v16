99e196709903e028fafa08381fb434b63d9ff742
Your Name <you@example.com>
2026-05-06 16:01:57 -0500

    upa

---
+++ credit_sensotek/models/sale_order.py
@@ -0,0 +1,28 @@
+from odoo import api, fields, models
+from datetime import date
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    def action_confirm(self):
+
+        limit_credit = self.partner_id.credit_limit
+
+        if self.parent_id:
+            limit_credit = self.partner_id.parent_id.credit_limit
+
+        if limit_credit != 0 :
+            today = date.today()
+
+            raise ValueError(today)
+
+            moves = self.env['account.move'].search([
+                ('invoice_date_due', '<', today),
+                ('state', '=', 'posted'),
+                ('invoice_payment_state', '=', 'not_paid')
+            ])
+
+        res = super().action_confirm()
+
+        return res
+
