432475191fb919d1b6483bd40b5e419ca742e6e0
Your Name <you@example.com>
2026-05-21 18:56:09 -0500

    multimedia

---
+++ tmp_fields_sensotek/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Fiedls TMP Sensotek",
+    "summary": """Fiedls TMP Sensotek""",
+    "description": """Fiedls TMP Sensotek""",
+    "author": "JZOLUTIONS",
+
+    "category": "Uncategorized",
+    "version": "0.1",
+    "depends": ["sale"],
+    # always loaded
+    "data": [
+        #'views/product_pricelist.xml',
+        #'views/product_category.xml',
+        #'views/sale_order.xml',
+    ],
+    # 'images': ['static/description/odoo-woo.gif'],
+}
