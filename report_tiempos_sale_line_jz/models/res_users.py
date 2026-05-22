846bd7bccfaa1258a8180973c86f9bcdb1abcad1
Your Name <you@example.com>
2026-05-21 19:30:19 -0500

    funcional

--- report_tiempos_sale_line_jz/models/res_users.py
+++ report_tiempos_sale_line_jz/models/res_users.py
@@ -2,4 +2,5 @@ from odoo import api, fields, models
 
 class ResUsers(models.Model):
     _inherit = 'res.users'
-    company_rstk_ids = fields.Many2many('res.company',string="Compañias Permitidas Reporte Sensotek")
+    company_rstk_ids = fields.Many2many('res.company',string="Compañias Reporte Sensotek Adicional")
+    partner_rstk_ids = fields.Many2many('res.partner', string="Contactos Reporte Sensotek Adicional")
