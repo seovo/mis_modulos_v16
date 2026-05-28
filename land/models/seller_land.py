bafd0e4c6819e84910bd136bc8530d91f0899709
Your Name <you@example.com>
2026-05-28 10:42:35 -0500

    ESTRUCTURA ODOO

--- land/models/seller_land.py
+++ land/models/seller_land.py
@@ -3,6 +3,8 @@ from odoo import api, fields, models , _
 class SellerLand(models.Model):
     _name          = 'seller.land'
     _description   = 'seller.land'
+
+    active         = fields.Boolean(default=True)
     name           = fields.Char()
     commission_percentage          = fields.Float()
     company_id = fields.Many2one('res.company', 'Company', required=True, index=True,
