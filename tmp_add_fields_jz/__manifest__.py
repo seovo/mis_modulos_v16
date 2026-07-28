8069f412233c18b488190614f76de48bb6832dea
Your Name <you@example.com>
2026-07-27 21:30:00 -0500

    UPA

---
+++ tmp_add_fields_jz/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Campos Temporales",
+    "summary": "Campos Temporales",
+    "description": "Campos Temporales",
+    "author": "Jzolutions",
+    "category": "Uncategorized",
+    "version": "1.1",
+    "depends": ["account"],
+    "data": [],
+    # 'uninstall_hook': 'uninstall_hook',
+    #"external_dependencies": {"python": ["msal"]},
+    "application": False,
+    "installable": True,
+    "auto_install": False,
+
+}
