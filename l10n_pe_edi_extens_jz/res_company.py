a2b0504ede1d16e810d92acc9934277b0bc46323
Your Name <you@example.com>
2026-07-28 11:37:23 -0500

    UPA

--- l10n_pe_edi_extens_jz/res_company.py
+++
@@ -1,6 +0,0 @@
-from odoo import _, api, fields, models
-
-class ResCompany(models.Model):
-    _inherit = 'res.company'
-    series_available_factu_jz = fields.Char(string='Series Permitidas')
-    automatic_factu_jz = fields.Boolean(default=True,string='Facturar Automaticamente')
