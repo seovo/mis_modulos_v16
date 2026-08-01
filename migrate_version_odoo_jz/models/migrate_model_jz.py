649cf37a8dcf0cfaec97056ea291b16b02d06f18
Your Name <you@example.com>
2026-07-31 20:50:16 -0500

    UPA

--- migrate_version_odoo_jz/models/migrate_model_jz.py
+++ migrate_version_odoo_jz/models/migrate_model_jz.py
@@ -512,7 +512,7 @@ class MigrateModelJz(models.Model):
                 name_group = taxgroup[1]
 
                 exist_tax_group = self.env['account.tax.group'].search([
-                    ('name', 'ilike', name_group),('country_id','=',self.company.country_id.id)
+                    ('name', 'ilike', name_group),('country_id','=',self.company_id.country_id.id)
                 ])
 
                 data_insert = {
