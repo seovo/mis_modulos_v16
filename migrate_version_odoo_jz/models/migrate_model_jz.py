6f89140a1966e727ef032c1ef1abe73fe39b2614
Your Name <you@example.com>
2026-08-01 12:55:14 -0500

    UPA

--- migrate_version_odoo_jz/models/migrate_model_jz.py
+++ migrate_version_odoo_jz/models/migrate_model_jz.py
@@ -861,6 +861,9 @@ class MigrateModelJz(models.Model):
                 ]
                 exist_tax = self.env['account.tax'].search(dominio_tax)
 
+                if value_name == '18% ITBIS Incl. Compras' :
+                    raise ValueError(exist_tax)
+
                 if len(exist_tax) > 1:
                     exist_tax = None
 
