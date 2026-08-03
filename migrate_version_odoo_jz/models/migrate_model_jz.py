32506c1676ae9b38667b78e480912ce8937a64cb
Your Name <you@example.com>
2026-08-03 13:17:45 -0500

    UPA

--- migrate_version_odoo_jz/models/migrate_model_jz.py
+++ migrate_version_odoo_jz/models/migrate_model_jz.py
@@ -769,7 +769,12 @@ class MigrateModelJz(models.Model):
 
                         })
 
-                    exist_diario = self.env['account.journal'].create(dict_create_journal)
+                    try:
+                        exist_diario = self.env['account.journal'].create(dict_create_journal)
+                    except:
+                        raise ValidationError(str(dict_create_journal))
+
+
 
                 if exist_diario:
                     data_insert.update({
