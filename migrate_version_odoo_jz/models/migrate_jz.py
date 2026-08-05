4f338a2727da641adba7978cd27d69dd2b3bc048
Your Name <you@example.com>
2026-08-05 13:41:59 -0500

    up

--- migrate_version_odoo_jz/models/migrate_jz.py
+++ migrate_version_odoo_jz/models/migrate_jz.py
@@ -496,7 +496,7 @@ class MigrateJz(models.Model):
         moves_without_name = self.env['account.move.line'].search([
             ('move_id', '!=', False),
             ('move_id.name', '!=', '/'),
-            ('move_name', '=', False)], limit=1500)
+            ('move_name', '=', False)], limit=2000)
 
         if moves_without_name:
             for mwn in moves_without_name:
