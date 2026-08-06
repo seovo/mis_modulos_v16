557f05a20956a8a3eea55185667b7b8d0c3fdebc
Your Name <you@example.com>
2026-08-05 22:07:59 -0500

    up

--- migrate_version_odoo_jz/models/migrate_model_jz.py
+++ migrate_version_odoo_jz/models/migrate_model_jz.py
@@ -1517,7 +1517,7 @@ END AS display_type      ''',
 
             return
 
-        raise ValidationError([self.table, self.last_value])
+
 
         if self.last_value <= 0 :
             if self.table == 'product_taxes_rel':
@@ -1576,6 +1576,7 @@ WHERE aml.id = sub.id;
                 '''
 
                 raise ValidationError(sql)
+                raise ValidationError([self.table, self.last_value])
                 self.env.cr.execute(sql)
 
 
