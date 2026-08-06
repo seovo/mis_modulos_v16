58b4099c363ec25e0dda9c909940b7d3e711a355
Your Name <you@example.com>
2026-08-05 22:20:52 -0500

    up

--- migrate_version_odoo_jz/models/migrate_jz.py
+++ migrate_version_odoo_jz/models/migrate_jz.py
@@ -558,7 +558,7 @@ class MigrateJz(models.Model):
             # ('payment_state','!=','reversed'),
             ('currency_id', '!=', False),
             ('line_ids', '!=', False),
-            ('line_ids.balance', '!=', 0)
+            #('line_ids.balance', '!=', 0)
             #('currency_id', '=', self.env.ref('base.PEN').id),
         ], limit=2000)
 
