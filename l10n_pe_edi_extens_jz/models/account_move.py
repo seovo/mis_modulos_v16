6391b3ac2d86aff74255f5f82118291e9422d315
Your Name <you@example.com>
2026-08-30 18:51:06 -0500

    up

--- l10n_pe_edi_extens_jz/models/account_move.py
+++ l10n_pe_edi_extens_jz/models/account_move.py
@@ -62,7 +62,7 @@ class AccountMove(models.Model):
         self.change_date_jz_jz()
         res = super().action_post()
         for record in self:
-            if record.company_id.automatic_factu_jz:
+            if record.company_id.automatic_factu_jz and record.journal_id.l10n_latam_use_documents:
 
                 if record.edi_document_ids:
                     record.button_process_edi_web_services()
