8835929936fe7eadedf5e7ba8af8910f8ddebdc0
Your Name <you@example.com>
2026-09-23 18:29:59 -0500

    identifications

--- migrate_version_odoo_jz/models/migrate_model_jz.py
+++ migrate_version_odoo_jz/models/migrate_model_jz.py
@@ -1764,10 +1764,10 @@ END AS display_type      ''',
                     self.env.cr.execute(SQL_CONSULTA, [value_invoice_id, value_name])
                     result = self.env.cr.fetchall()
 
-                #if value_id == 9184:
-                #    SQL_CONSULTA = f"SELECT  id FROM  {table} WHERE  x_invoice_id = %s "
-                #    self.env.cr.execute(SQL_CONSULTA, [value_invoice_id])
-                #    raise ValueError(result)
+                if value_id == 9184:
+                    SQL_CONSULTA = f"SELECT  id FROM  {table} WHERE  x_invoice_id = %s "
+                    self.env.cr.execute(SQL_CONSULTA, [value_invoice_id])
+                    raise ValueError(result)
 
                 if len(result) == 0:
                     #BUSCAR SOLO PRODUCTO
